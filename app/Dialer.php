b51c0645515a14cb7f29d52a1a7ffa24979da71a
Bryan Burchfield <bburchfield@gmail.com>
2019-09-16 14:53:44 -0400

    Dialer model

--- app/Dialer.php
+++ app/Dialer.php
@@ -6,7 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Dialer extends Model
 {
-    // set db and actual table name
-    protected $connection = 'sqlsrv';
-    protected $table = 'dialers';
+    //
 }
