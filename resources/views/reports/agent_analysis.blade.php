dfef723a63de4d91fee2c12217f711973b472e69
Thomas Grauer <tgrauer1@me.com>
2019-09-20 13:27:27 -0400

    Add include file for report db_menu

--- resources/views/reports/agent_analysis.blade.php
+++ resources/views/reports/agent_analysis.blade.php
@@ -9,24 +9,7 @@
 
 			<div class="row">
 
-				@if(Auth::user()->isMultiDb())
-					@php $show_multi_db = 'show_multi_db'; @endphp
-				@else
-					@php $show_multi_db = ''; @endphp
-				@endif
-
-				<div class="col-sm-4 multi_db {{ $show_multi_db }}">
-					<div class="form-group">
-						<label>Database</label>
-			            <select name="databases[]" id="database_select" multiple class="form-control multiselect" value="<?php if(isset($_POST['databases'])){echo $_POST['databases'];}?>">
-
-							@foreach ($filters['db_list'] as $key => $value) {
-				                <option value="{{$value}}">{{$key}}</option>
-				            @endforeach
-
-						</select>
-			        </div>
-				</div>
+				@include('shared.report_db_menu')
 					
 				<div class="col-sm-4">
 					<div class="form-group">
--- resources/views/reports/lead_inventory_sub.blade.php
+++ resources/views/reports/lead_inventory_sub.blade.php
@@ -8,24 +8,7 @@
 		{!! Form::open(['method'=>'POST', 'url'=> '#', 'name'=>'report_filter_form', 'id'=>$report, 'class'=>'report_filter_form']) !!}
 
 			<div class="row">
-				@if(Auth::user()->isMultiDb())
-					@php $show_multi_db = 'show_multi_db'; @endphp
-				@else
-					@php $show_multi_db = ''; @endphp
-				@endif
-
-				<div class="col-sm-4 multi_db {{ $show_multi_db }}">
-					<div class="form-group">
-						<label>Database</label>
-			            <select name="databases[]" id="database_select" multiple class="form-control multiselect" value="<?php if(isset($_POST['databases'])){echo $_POST['databases'];}?>">
-
-							@foreach ($filters['db_list'] as $key => $value) {
-				                <option value="{{$value}}">{{$key}}</option>
-				            @endforeach
-
-						</select>
-			        </div>
-				</div>
+				@include('shared.report_db_menu')
 				
 				<div class="col-sm-4">
 					<div class="form-group">
