72461f0049e5adfdac9e3db2efb370cd3a7c4963
Bryan Burchfield <bburchfield@gmail.com>
2019-09-23 14:19:14 -0400

    KPI query update

--- database/migrations/2019_08_29_174740_create_kpis_table.php
+++ database/migrations/2019_08_29_174740_create_kpis_table.php
@@ -137,7 +137,7 @@ GROUP BY ROLLUP(Campaign)",
                     'query' => "SELECT 'Agent' = ISNULL(Rep, 'Total'),
 'Calls' = COUNT(id),
 'Call-Time' = CAST(DATEADD(SECOND, SUM(CASE WHEN Action IN ('InboundCall','Call','ManualCall') THEN Duration ELSE 0 END), 0) AS TIME(0)),
-'After-Call' = CAST(DATEADD(SECOND, SUM(CASE WHEN Action IN ('Disposition') THEN Duration ELSE 0 END), 0) AS TIME(0)),
+'Disposition' = CAST(DATEADD(SECOND, SUM(CASE WHEN Action IN ('Disposition') THEN Duration ELSE 0 END), 0) AS TIME(0)),
 'Average Handle Time' = CAST(DATEADD(SECOND, (SUM(Duration) / COUNT(Rep)), 0) AS TIME (0))
 FROM AgentActivity
 WHERE Action IN ('InboundCall','Call','ManualCall','Disposition')
