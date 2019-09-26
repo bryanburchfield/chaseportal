f32806c217b7974175bbebefc8ec75a1f643c1a8
Bryan Burchfield <bburchfield@gmail.com>
2019-09-26 13:47:36 -0400

    fixed routes

--- routes/dashboards.php
+++ routes/dashboards.php
@@ -11,17 +11,17 @@ Route::prefix('dashboards')->group(function () {
     // must be logged in to access any of these
     Route::group(['middleware' => 'auth'], function () {
         Route::get('/', 'MasterDashController@index');
-        Route::post('showreport', 'MasterDashController@showReport');
         Route::get('settings', 'MasterDashController@showSettings');
         Route::post('settings', 'MasterDashController@updateUserSettings');
+        Route::get('showreport', 'MasterDashController@showReport');
 
         // Reports
-        Route::get('automatedreports', 'AutomatedReportController@automatedReports');
-        Route::get('reports/{report}', 'ReportController@index');
-        Route::post('reports/{report}', 'ReportController@runReport');
-        Route::post('toggle_automated_report', 'AutomatedReportController@toggleAutomatedReport');
         Route::post('reports/get_campaigns', 'ReportController@getCampaigns');
         Route::post('reports/get_subcampaigns', 'ReportController@getSubcampaigns');
+        Route::get('automatedreports', 'AutomatedReportController@automatedReports');
+        Route::post('toggle_automated_report', 'AutomatedReportController@toggleAutomatedReport');
+        Route::get('reports/{report}', 'ReportController@index');
+        // Route::post('reports/{report}', 'ReportController@runReport');
 
         // ajax targets
         Route::post('set_dashboard', 'MasterDashController@setDashboard');
