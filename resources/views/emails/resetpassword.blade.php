3c51cb4a551c3494e58533d8ec275da2a9311ba5
Bryan Burchfield <bburchfield@gmail.com>
2019-09-30 15:00:46 -0400

    forgot password email

--- resources/views/emails/resetpassword.blade.php
+++ resources/views/emails/resetpassword.blade.php
@@ -103,7 +103,7 @@
 
                         <td valign='middle' style='text-align:center;padding:0 10px 10px 0'>
                             <p>You are receiving this email because we received a password reset request for your account.</p>
-							<a href="{{$link}}" class="btn btn-md">Reset Password</a>
+							<a href="{{ $data['link'] }}" class="btn btn-md">Reset Password</a>
                             <p>This password reset link will expire in 60 minutes.</p>
                         	<p>If you did not request a password reset, no further action is required.</p>
                         </td>
