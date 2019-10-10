43037f6d8f24793da18d198c3e74d92381ae97ea
Bryan Burchfield <bburchfield@gmail.com>
2019-10-10 11:05:12 -0400

    allow old md5 passwords

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\User;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Support\Facades\Session;
 use Auth;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
@@ -45,5 +47,29 @@ class LoginController extends Controller
         Auth::logout();
         Session::flush();
         return redirect()->back();
+    }
+
+    /**
+     * Attempt Login
+     * Check for old md5 hashed pw and update in db if so
+     *
+     * @param Request $request
+     * @return void
+     */
+    protected function attemptLogin(Request $request)
+    {
+        // see if the entered password matches md5 and update if so
+        $user = User::where('email', $request->email)->first();
+
+        if ($user && $user->password == md5($request->password)) {
+            $user->password = Hash::make($request->password);
+            $user->save();
+        }
+
+        // Continue as normal
+        return $this->guard()->attempt(
+            $this->credentials($request),
+            $request->filled('remember')
+        );
     }
 }
