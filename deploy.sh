5e696bf16bd22eb53ccbdb7c5f027a81891679e7
Bryan Burchfield <bburchfield@gmail.com>
2019-10-23 10:34:10 -0400

    Minor mods

--- deploy.sh
+++ deploy.sh
@@ -8,23 +8,25 @@ git pull
 
 # update PHP dependencies
 export COMPOSER_HOME='/tmp/composer'
-composer install --no-interaction --no-dev --prefer-dist
-	# --no-interaction	Do not ask any interactive question
-	# --no-dev		Disables installation of require-dev packages.
-	# --prefer-dist		Forces installation from package dist even for dev versions.
+composer install --no-interaction --no-dev --prefer-dist --optimize-autoloader
+	# --no-interaction	    Do not ask any interactive question
+	# --no-dev		        Disables installation of require-dev packages.
+	# --prefer-dist		    Forces installation from package dist even for dev versions.
+    # --optimize-autoloader Optimize autoload files
 
 # update database
 php artisan migrate --force
 	# --force		Required to run when in production.
 
-# clear cache
+# clear caches
 php artisan cache:clear
 
-# clear config cache
-php artisan config:clear
+# Clear expired password reset tokens
+php artisan auth:clear-resets
 
-# cache config
-php artisan config:cache
+# recache everything - 'optimize' caches config and routes
+php artisan optimize
+php artisan view:cache
 
 # restart queues
 php artisan -v queue:restart
