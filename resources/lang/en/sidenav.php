4d941e822bc3e372cf25a1dd06133b7e10c8f64b
Thomas Grauer <tgrauer1@me.com>
2019-10-29 12:16:50 -0400

    Spanish translations for sidenav

---
+++ resources/lang/en/sidenav.php
@@ -0,0 +1,14 @@
+<?php 
+
+	return [
+		'admin_inbound'		=> 'Admin Inbound',
+		'admin_outbound'	=> 'Admin Outbound',
+		'trend_dashboard'	=> 'Trend Dashboard',
+		'leaderboard'		=> 'Leaderboard',
+		'kpis'				=> 'KPIs',
+		'reports'			=> 'Reports',
+		'setting'			=> 'Settings',
+		'tools'				=> 'Tools',
+		'admin'				=> 'Admin'
+	];
+?>
---
+++ resources/lang/es/sidenav.php
@@ -0,0 +1,14 @@
+<?php 
+
+	return [
+		'admin_inbound'		=> 'Administrador Entrante',
+		'admin_outbound'	=> 'Administrador Saliente',
+		'trend_dashboard'	=> 'Trend Dashboard',
+		'leaderboard'		=> 'Panel de tendencia',
+		'kpis'				=> 'KPIs',
+		'reports'			=> 'Informes',
+		'setting'			=> 'Configuraciones',
+		'tools'				=> 'Herramientas',
+		'admin'				=> 'Administración'
+	];
+?>
