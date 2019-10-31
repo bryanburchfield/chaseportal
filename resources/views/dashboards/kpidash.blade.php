1dda275208dd639062dbe2dc11b9c3b0e48ddc26
Thomas Grauer <tgrauer1@me.com>
2019-10-31 09:50:14 -0400

    Translated KPI and recip pages (not kpi names/descrip)

--- resources/views/dashboards/kpidash.blade.php
+++ resources/views/dashboards/kpidash.blade.php
@@ -3,7 +3,7 @@
 <div class="container-full mt20">
     <div class="row">
         <div class="col-sm-12">
-            <h2>KPI Notifications</h2>
+            <h2>{{__('kpi.kpi_notifications')}}</h2>
         </div>
 
         @foreach(\App\Kpi::getKpis() as $kpi)
@@ -12,7 +12,7 @@
             <a href="#" class="kpi_trigger"> {{ $kpi->name }}</a>
 
             <div class="controls">
-                <a href="https://webdev.chasedatacorp.com/kpi/crons/cron_{{ $kpi->id}}.php?interval={{ $kpi->interval }}" class="run_kpi btn btn-default btn-sm"><span class="glyphicon glyphicon-flash"></span> Run Now</a>
+                <a href="https://webdev.chasedatacorp.com/kpi/crons/cron_{{ $kpi->id}}.php?interval={{ $kpi->interval }}" class="run_kpi btn btn-default btn-sm"><span class="glyphicon glyphicon-flash"></span> {{__('kpi.run_now')}}</a>
                 <label class="switch">
                     <input type="checkbox" {{ ($kpi->active) ? 'checked' : '' }} name="kpi_input">
                     <span></span>
@@ -23,30 +23,30 @@
                 <p>{{ $kpi->description }}</p>
                 <div class="row mt30 options kpi_options_top">
                     <div class="col-sm-4">
-                        <h4 class="expand_dets"><i class="glyphicon glyphicon-wrench exp"></i> Options</h4>
+                        <h4 class="expand_dets"><i class="glyphicon glyphicon-wrench exp"></i> {{__('kpi.options')}}</h4>
                         <div class="expanded_options clear card">
                             <form data-kpi="{{ $kpi->id }}" action="#" method="post" class="form adjust_interval">
                                 <div class="form-group">
-                                    <label for="type">Interval</label>
+                                    <label for="type">{{__('kpi.interval')}}</label>
                                     <select name="interval" class="form-control interval"  required>
-                                        <option {!! ($kpi->interval == '1440') ? ' selected="selected"' : '' !!} value="1440">Once a Day</option>
-                                        <option {!! ($kpi->interval == '720') ? ' selected="selected"' : '' !!} value="720">Twice a Day</option>
-                                        <option {!! ($kpi->interval == '60') ? ' selected="selected"' : '' !!} value="60">Every Hour</option>
-                                        <option {!! ($kpi->interval == '30') ? ' selected="selected"' : '' !!} value="30">Every 30 minutes</option>
-                                        <option {!! ($kpi->interval == '15') ? ' selected="selected"' : '' !!} value="15">Every 15 minutes</option>
+                                        <option {!! ($kpi->interval == '1440') ? ' selected="selected"' : '' !!} value="1440">{{__('kpi.one_a_day')}}</option>
+                                        <option {!! ($kpi->interval == '720') ? ' selected="selected"' : '' !!} value="720">{{__('kpi.twice_a_day')}}</option>
+                                        <option {!! ($kpi->interval == '60') ? ' selected="selected"' : '' !!} value="60">{{__('kpi.hourly')}}</option>
+                                        <option {!! ($kpi->interval == '30') ? ' selected="selected"' : '' !!} value="30">{{__('kpi.every_30')}}</option>
+                                        <option {!! ($kpi->interval == '15') ? ' selected="selected"' : '' !!} value="15">{{__('kpi.every_15')}}</option>
                                     </select>
                                 </div>
-                                <input type="submit" class="btn btn-primary btn-md mb0" value="Save">
+                                <input type="submit" class="btn btn-primary btn-md mb0" value="{{__('general.save')}}">
                             </form>
                         </div>
                     </div>
 
                     <div class="col-sm-4">
-                        <h4 class="expand_dets"><i class="glyphicon glyphicon-envelope"></i> Recipients</h4>
+                        <h4 class="expand_dets"><i class="glyphicon glyphicon-envelope"></i> {{__('kpi.recipients')}}</h4>
                         <div class="expanded_emails clear">
 
                             @if(!count($kpi->recipients))
-                                <div class="alert alert-info">No Recipients have been added yet</div>
+                                <div class="alert alert-info"> {{__('kpi.no_recips')}}</div>
                             @else
                                 @foreach($kpi->recipients as $r)
 
@@ -80,18 +80,18 @@
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title" id="myModalLabel">Confirm Recipient Removal</h4>
+                <h4 class="modal-title" id="myModalLabel">{{__('kpi.confirm_recipient_removal')}}</h4>
             </div>
             <div class="modal-body">
                 <input type="hidden" class="user_id" name="user_id" value="">
                 <input type="hidden" class="name" name="name" value="">
                 <input type="hidden" class="fromall" name="fromall" value="0">
                 <input type="hidden" class="kpi_id" name="kpi_id" value="">
-               <h3>Are you sure you want to remove <span class="username"></span> from this KPI?</h3>
+               <h3>{{__('kpi.are_you_sure')}} <span class="username"></span> {{__('kpi.from_this_kpi')}}?</h3>
             </div>
         <div class="modal-footer">
-            <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-            <button type="button" class="btn btn-danger remove_recip">Remove Recipient</button>
+            <button type="button" class="btn btn-default" data-dismiss="modal">{{__('general.cancel')}}</button>
+            <button type="button" class="btn btn-danger remove_recip">{{__('kpi.remove_recipient')}}</button>
         </div>
     </div>
     </div>
