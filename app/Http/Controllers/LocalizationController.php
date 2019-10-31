0a74f058a47a7f94343c3ef883cec03a350b7a1e
Bryan Burchfield <bburchfield@gmail.com>
2019-10-31 10:21:38 -0400

    Save user language

--- app/Http/Controllers/LocalizationController.php
+++ app/Http/Controllers/LocalizationController.php
@@ -4,13 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App;
+use Illuminate\Support\Facades\Auth;
 
 class LocalizationController extends Controller
 {
     public function lang($locale)
     {
-        App::setLocale($locale);
-        session()->put('locale', $locale);
+        if (in_array($user->language, config('localization.locales'))) {
+            App::setLocale($locale);
+            session()->put('locale', $locale);
+
+            if (Auth::check()) {
+                $user = Auth::user();
+                $user->language = $locale;
+                $user->save();
+            }
+        }
         return redirect()->back();
     }
 }
