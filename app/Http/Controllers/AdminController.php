9bb37f01476450721b7eda5676ea8c706c00c9f1
Bryan Burchfield <bburchfield@gmail.com>
2020-01-17 10:28:12 -0500

    Log out/it after 'edit myself'

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -212,6 +212,8 @@ class AdminController extends Controller
         try {
             $user = Auth::user();
             $user->update($request->all());
+            Auth::logout();
+            Auth::login($user);
         } catch (Exception $e) {
             return ['errors' => ['Update Failed']];
         }
