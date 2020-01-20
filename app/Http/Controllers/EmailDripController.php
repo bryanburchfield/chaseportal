e6c6b9eb8c3e4921cfed6cef6e4f27da575f2178
Bryan Burchfield <bburchfield@gmail.com>
2020-01-20 12:03:16 -0500

    ESP index

--- app/Http/Controllers/EmailDripController.php
+++ app/Http/Controllers/EmailDripController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\EmailServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class EmailDripController extends Controller
 {
+    /**
+     * Email Drip Campaign index
+     * 
+     * @return Illuminate\View\View|Illuminate\Contracts\View\Factory 
+     */
     public function index()
     {
         $page = [
@@ -27,11 +33,22 @@ class EmailDripController extends Controller
         # code...
     }
 
+    /**
+     * Email Service Providers index
+     * 
+     * @return Illuminate\View\View|Illuminate\Contracts\View\Factory 
+     */
     public function espIndex()
     {
+        $email_service_providers = EmailServiceProvider::where('group_id', Auth::User()->group_id)
+            ->orderby('name')
+            ->get();
+
         $page = [
             'menuitem' => 'tools',
             'type' => 'other',
+            'providers' => $this->getProviders(),
+            'email_service_providers' => $email_service_providers,
         ];
 
         $data = [
@@ -50,5 +67,19 @@ class EmailDripController extends Controller
     public function uploadTemplate(Request $request)
     {
         # code...
+    }
+
+    /**
+     * Get Providers
+     * 
+     * Returns a collection of supported ESPs
+     * May need to tableize this later and add other fields
+     * @return string[] 
+     */
+    private function getProviders()
+    {
+        return [
+            'postmark',
+        ];
     }
 }
