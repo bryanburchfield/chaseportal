1440d0a17b415c692a3489973e9675a7999ac1fc
Bryan Burchfield <bburchfield@gmail.com>
2020-01-22 08:22:41 -0500

    Fixed controller names

--- routes/inbounddashboard.php
+++ routes/inbounddashboard.php
@@ -2,23 +2,23 @@
 // Admin Dashboard: all urls start with /inbounddashboard/
 Route::prefix('inbounddashboard')->group(function () {
     // Allow app_token login via /inbounddashboard/api/{token}
-    Route::get('/', 'inboundDashController@apiLogin');
-    Route::get('api/{token}', 'inboundDashController@apiLogin');
+    Route::get('/', 'InboundDashController@apiLogin');
+    Route::get('api/{token}', 'InboundDashController@apiLogin');
     Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
 
     // must be logged in to access any of these
     Route::group(['middleware' => 'auth'], function () {
 
         // ajax targets
-        Route::post('update_filters', 'inboundDashController@updateFilters');
-        Route::post('call_volume', 'inboundDashController@callVolume');
-        Route::post('total_sales', 'inboundDashController@totalSales');
-        Route::post('avg_hold_time', 'inboundDashController@avgHoldTime');
-        Route::post('abandon_rate', 'inboundDashController@abandonRate');
-        Route::post('agent_call_count', 'inboundDashController@agentCallCount');
-        Route::post('agent_call_status', 'inboundDashController@agentCallStatus');
-        Route::post('service_level', 'inboundDashController@serviceLevel');
-        Route::post('rep_avg_handletime', 'inboundDashController@repAvgHandleTime');
-        Route::post('agent_call_status', 'inboundDashController@agentCallStatus');
+        Route::post('update_filters', 'InboundDashController@updateFilters');
+        Route::post('call_volume', 'InboundDashController@callVolume');
+        Route::post('total_sales', 'InboundDashController@totalSales');
+        Route::post('avg_hold_time', 'InboundDashController@avgHoldTime');
+        Route::post('abandon_rate', 'InboundDashController@abandonRate');
+        Route::post('agent_call_count', 'InboundDashController@agentCallCount');
+        Route::post('agent_call_status', 'InboundDashController@agentCallStatus');
+        Route::post('service_level', 'InboundDashController@serviceLevel');
+        Route::post('rep_avg_handletime', 'InboundDashController@repAvgHandleTime');
+        Route::post('agent_call_status', 'InboundDashController@agentCallStatus');
     });
 });
--- routes/outbounddashboard.php
+++ routes/outbounddashboard.php
@@ -2,27 +2,27 @@
 // Admin Outbound Dashboard: all urls start with /outbounddashboard/
 Route::prefix('outbounddashboard')->group(function () {
     // Allow app_token login via /outbounddashboard/api/{token}
-    Route::get('/', 'outboundDashController@apiLogin');
-    Route::get('api/{token}', 'outboundDashController@apiLogin');
+    Route::get('/', 'OutboundDashController@apiLogin');
+    Route::get('api/{token}', 'OutboundDashController@apiLogin');
     Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
 
     // must be logged in to access any of these
     Route::group(['middleware' => 'auth'], function () {
 
         // ajax targets
-        Route::post('update_filters', 'outboundDashController@updateFilters');
-        Route::post('call_volume', 'outboundDashController@callVolume');
-        Route::post('completed_calls', 'outboundDashController@completedCalls');
-        Route::post('avg_hold_time', 'outboundDashController@avgHoldTime');
-        Route::post('avg_wait_time', 'outboundDashController@avgWaitTime');
-        Route::post('abandon_rate', 'outboundDashController@abandonRate');
-        Route::post('agent_call_count', 'outboundDashController@agentCallCount');
-        Route::post('service_level', 'outboundDashController@serviceLevel');
-        Route::post('rep_avg_handletime', 'outboundDashController@repAvgHandleTime');
-        Route::post('agent_talk_time', 'outboundDashController@agentTalkTime');
-        Route::post('sales_per_hour_per_rep', 'outboundDashController@salesPerHourPerRep');
-        Route::post('calls_by_campaign', 'outboundDashController@callsByCampaign');
-        Route::post('total_calls', 'outboundDashController@totalCalls');
-        Route::post('agent_call_status', 'outboundDashController@agentCallStatus');
+        Route::post('update_filters', 'OutboundDashController@updateFilters');
+        Route::post('call_volume', 'OutboundDashController@callVolume');
+        Route::post('completed_calls', 'OutboundDashController@completedCalls');
+        Route::post('avg_hold_time', 'OutboundDashController@avgHoldTime');
+        Route::post('avg_wait_time', 'OutboundDashController@avgWaitTime');
+        Route::post('abandon_rate', 'OutboundDashController@abandonRate');
+        Route::post('agent_call_count', 'OutboundDashController@agentCallCount');
+        Route::post('service_level', 'OutboundDashController@serviceLevel');
+        Route::post('rep_avg_handletime', 'OutboundDashController@repAvgHandleTime');
+        Route::post('agent_talk_time', 'OutboundDashController@agentTalkTime');
+        Route::post('sales_per_hour_per_rep', 'OutboundDashController@salesPerHourPerRep');
+        Route::post('calls_by_campaign', 'OutboundDashController@callsByCampaign');
+        Route::post('total_calls', 'OutboundDashController@totalCalls');
+        Route::post('agent_call_status', 'OutboundDashController@agentCallStatus');
     });
 });
