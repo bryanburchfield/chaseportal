fe3d5f4cdd996dcfbe1e0481cc979b8b1c850ecc
Bryan Burchfield <bburchfield@gmail.com>
2020-01-24 08:48:58 -0500

    Drip campaign validation

--- app/Http/Requests/ValidEmailDripCampaign.php
+++ app/Http/Requests/ValidEmailDripCampaign.php
@@ -25,18 +25,21 @@ class ValidEmailDripCampaign extends FormRequest
      */
     public function rules()
     {
+        $group_id = Auth::User()->group_id;
+
         return [
             'name' => 'required',
             'description' => 'required',
             'campaign' => 'required',
             'subcampaign' => 'nullable',
             'email_field' => 'required',
-            'smtp_server_id' => 'required',
-            // Rule::exists('smtp_servers')->where(function ($query) {
-            //     $query
-            //         ->where('smtp_server_id', $this->smtp_server_id)
-            //         ->where('group_id', Auth::User()->group_id);
-            // }),
+            'smtp_server_id' => [
+                'required',
+                Rule::exists('smtp_servers', 'id')
+                    ->where(function ($query) use ($group_id) {
+                        $query->where('group_id', $group_id);
+                    }),
+            ],
             'template_id' => 'required|integer',
         ];
     }
