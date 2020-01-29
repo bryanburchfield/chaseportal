13b38c37f3e2f91fe36fecee2770e845b7b55df1
Bryan Burchfield <bburchfield@gmail.com>
2020-01-29 12:34:50 -0500

    Added client/user counts to dialer lists

--- app/Models/Dialer.php
+++ app/Models/Dialer.php
@@ -25,4 +25,26 @@ class Dialer extends Model
 
         return $users;
     }
+
+    public function group_count($include_additional = false)
+    {
+        if ($include_additional) {
+            $count = User::where(
+                function ($query) {
+                    $query->where('db', $this->reporting_db)
+                        ->orWhere('additional_dbs', $this->reporting_db);
+                }
+            )
+                ->whereNotIn('user_type', ['demo', 'expired'])
+                ->distinct('group_id')
+                ->count();
+        } else {
+            $count = User::where('db', $this->reporting_db)
+                ->whereNotIn('user_type', ['demo', 'expired'])
+                ->distinct('group_id')
+                ->count();
+        }
+
+        return $count;
+    }
 }
--- resources/views/shared/dialerlist.blade.php
+++ resources/views/shared/dialerlist.blade.php
@@ -1,12 +1,17 @@
+@php
+    // Really shouldn't put logic inside blades...
+    $tot_user_count = App\Models\User::whereNotIn('user_type', ['demo','expired'])->count();
+    $tot_client_count = App\Models\User::whereNotIn('user_type', ['demo','expired'])->distinct('group_id')->count();
+@endphp
 <div class="col-sm-6 pr0 mbmt50 mbp0">
-<h2 class="page_heading mb0">All Clients ({{ App\Models\User::count() }} total)</h2>
+<h2 class="page_heading mb0">All Clients ({{ $tot_client_count }}) All Users ({{ $tot_user_count }}) </h2>
     <div class="users">
         <div class="panel-group" id="{{$mode}}_accordion" role="tablist" aria-multiselectable="true">
         @foreach (App\Models\Dialer::orderBy('dialer_numb')->get() as $dialer)
             @php
             $db = sprintf("%02d", $dialer->dialer_numb);
             $users = $dialer->users(true);
-            $clients = $users->count();
+            $client_count = $dialer->group_count(true);
             @endphp
 
             <div class="panel panel-default">
@@ -15,8 +20,8 @@
                         <a class="collapsed" role="button" data-toggle="collapse" data-parent="#{{$mode}}_accordion" href="#{{$mode}}_dialer{{$db}}" aria-expanded="false" aria-controls="{{$mode}}_dialer{{$db}}">
                         Dialer {{$db}}
 
-                        @if($clients)
-                            ({{ $clients }} client{{ $clients > 1 ? 's' : '' }})
+                        @if($client_count)
+                            (Clients: {{ $client_count }}, Users: {{ $users->count() }})
                         @endif
                         </a>
                     </h4>
