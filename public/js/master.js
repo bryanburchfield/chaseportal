d8c306fe0b3e7d7efec637b44d1b19e48dbf7ebf
Thomas Grauer <tgrauer1@me.com>
2020-01-29 11:11:58 -0500

    test connection

--- public/js/master.js
+++ public/js/master.js
@@ -2222,13 +2222,8 @@ var Master = {
             success: function (response) {
                 console.log(response.properties);
                 $('#editServerModal .name').val(response.name);
-                $('#editServerModal .provider_type').val(response.provider_type)
-                // $('#editServerModal .host').val(response.host);
-                // $('#editServerModal .port').val(response.port);
-                // $('#editServerModal .username').val(response.username);
-                // $('#editServerModal .password').val(response.password);
+                $('#editServerModal .provider_type').val(response.provider_type);
                 $('#editServerModal .id').val(response.id);
-
                 $('#editServerModal .properties').empty();
                 var property_inputs='';
 
@@ -2237,8 +2232,6 @@ var Master = {
                     var label = key.charAt(0).toUpperCase() + key.slice(1);
                     property_inputs+='<div class="form-group"><label>'+label+'</label><input type="text" class="form-control '+key+'" name="properties['+key+']" value="'+value+'" required></div>';
                 }
-
-                console.log(property_inputs);
 
                 $('#editServerModal .properties').append(property_inputs);
             }
@@ -2289,13 +2282,7 @@ var Master = {
         $('.alert').empty().hide();
 
         var that = $(this).parent();
-
-        var host = $(that).find('.host').val()
-            name = $(that).find('.name').val(),
-            port = $(that).find('.port').val(),
-            username = $(that).find('.username').val(),
-            password = $(that).find('.password').val()
-        ;
+        var form_data = $(that).serialize();
 
         $.ajaxSetup({
             headers: {
@@ -2306,13 +2293,7 @@ var Master = {
         $.ajax({
             url: '/tools/email_drip/test_connection ',
             type: 'POST',
-            data: {
-                host: host,
-                name:name,
-                port:port,
-                username:username,
-                password:password
-            },
+            data: form_data,
             success: function (response) {
 
                 $(that).find('.test_connection').find('i').remove();
@@ -2361,7 +2342,7 @@ var Master = {
                 'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
             }
         });
-        console.log(id);
+
         $.ajax({
             url: '/tools/email_drip/delete_esp',
             type: 'POST',
@@ -2571,7 +2552,7 @@ var Master = {
                 success: function (response) {
                     $('.properties').empty();
                     var properties='';
-                    console.log(response);
+
                     response.forEach(function(item, index){
                         var label = item.charAt(0).toUpperCase() + item.slice(1);
                         properties+='<div class="form-group"><label>'+label+'</label><input type="text" class="form-control '+item+'" name="properties['+item+']" value="" required></div>';
