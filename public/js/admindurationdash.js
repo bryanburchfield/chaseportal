2ea919ac2808568177a195cfc0f4ddecb8b7731d
Thomas Grauer <tgrauer1@me.com>
2020-02-12 10:53:46 -0500

    Rearranging blades

---
+++ public/js/admindurationdash.js
@@ -0,0 +1,95 @@
+Chart.pluginService.register({
+    beforeDraw: function (chart) {
+        if (chart.config.options.elements.center) {
+            //Get ctx from string
+            var ctx = chart.chart.ctx;
+
+            //Get options from the center object in options
+            var centerConfig = chart.config.options.elements.center;
+            var fontStyle = centerConfig.fontStyle || 'Arial';
+            var txt = centerConfig.text;
+            var color = Master.tick_color;
+            var sidePadding = centerConfig.sidePadding || 20;
+            var sidePaddingCalculated = (sidePadding / 100) * (chart.innerRadius * 2)
+            //Start with a base font of 30px
+            ctx.font = "40px " + fontStyle;
+
+            //Get the width of the string and also the width of the element minus 10 to give it 5px side padding
+            var stringWidth = ctx.measureText(txt).width;
+            var elementWidth = (chart.innerRadius * 2) - sidePaddingCalculated;
+
+            // Find out how much the font can grow in width.
+            var widthRatio = elementWidth / stringWidth;
+            var newFontSize = Math.floor(20 * widthRatio);
+            var elementHeight = (chart.innerRadius * 2);
+
+            // Pick a new font size so it will not be larger than the height of label.
+            var fontSizeToUse = Math.min(newFontSize, elementHeight);
+
+            //Set font settings to draw it correctly.
+            ctx.textAlign = 'center';
+            ctx.textBaseline = 'top';
+            var centerX = ((chart.chartArea.left + chart.chartArea.right) / 2);
+            var centerY = ((chart.chartArea.top + chart.chartArea.bottom) / 1.7);
+            ctx.font = fontSizeToUse + "px " + fontStyle;
+            ctx.fillStyle = color;
+
+            //Draw text in center
+            ctx.fillText(txt, centerX, centerY);
+        }
+    }
+});
+
+var DurationDash = {
+
+	chartColors: {
+	    red: 'rgb(255,67,77)',
+	    blue: 'rgb(1,1,87)',
+	    orange: 'rgb(228,154,49)',
+	    green: 'rgb(51,160,155)',
+	    grey: 'rgb(98,98,98)',
+	    yellow: 'rgb(255, 205, 86)',
+	    lightblue: 'rgb(66, 134, 244)'
+	},
+
+	datefilter: document.getElementById("datefilter").value,
+	databases: '',
+	time: new Date().getTime(),
+
+	init:function(){
+
+		/// dashboard widgets
+		$.when(this.call_volume(this.datefilter)).done(function () {
+		    $('.preloader').fadeOut('slow');
+		    Master.check_reload();
+		});
+	},
+
+	call_volume:function(datefilter){
+		var campaign = $('.filter_campaign li ').text();
+
+		$.ajaxSetup({
+			headers: {
+				'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
+			}
+		});
+
+		$.ajax({
+			url: '/admindurationdashboard/call_volume',
+			type: 'POST',
+			dataType: 'json',
+			data: {
+				datefilter:datefilter,
+				campaign: campaign
+			},
+
+			success: function (response) {
+				console.log(response);
+			}
+		});
+	},
+}
+
+$(document).ready(function(){
+	DurationDash.init();
+});
--- resources/views/dashboards/admindurationdash.blade.php
+++ resources/views/dashboards/admindurationdash.blade.php
@@ -3,95 +3,88 @@
 
 @section('content')
 <div class="preloader"></div>
-<?php
-	//dd($default_lead_fields);
-?>
-<div class="wrapper">
 
-	@include('shared.admin_sidenav')
+<input type="hidden" value="{{ $dateFilter }}" id="datefilter" name="datefilter">
+@include('shared.navbar')
 
-	<div id="content">
+<div class="container-fluid bg">
+    <div class="container mt50">
+			<div class="row">
+			    <div class="col-sm-12">
+			        <div class="filter_time_camp_dets">
+			            <p>
+			                <span class="selected_datetime"></span> |
+			                <span class="selected_campaign"></span>
+			            </p>
+			        </div>
+			    </div>
+			</div>
 
-		@include('shared.navbar')
+			<div class="row">
+			    <div class="col-sm-3 col-xs-12">
+			        <div class="card-3 card" id="connect">
 
-		<div class="container-fluid bg dashboard p20">
-			<div class="container-full mt20">
-				<div class="row">
-				    <div class="col-sm-12">
-				        <div class="filter_time_camp_dets">
-				            <p>
-				                <span class="selected_datetime"></span> |
-				                <span class="selected_campaign"></span>
-				            </p>
-				        </div>
-				    </div>
-				</div>
+			            <div class="trend_indicator">
+			                <div class="trend_arrow"></div>
+			                <span></span>
+			            </div>
+			            <h1 class="title">{{__('widgets.connect')}} %</h1>
+			            <h4 class="data total mt30"></h4>
 
-				<div class="row">
-				    <div class="col-sm-3 col-xs-12">
-				        <div class="card-3 card" id="connect">
+			        </div><!-- end card -->
+			    </div><!-- end column -->
 
-				            <div class="trend_indicator">
-				                <div class="trend_arrow"></div>
-				                <span></span>
-				            </div>
-				            <h1 class="title">{{__('widgets.connect')}} %</h1>
-				            <h4 class="data total mt30"></h4>
+			    <div class="col-sm-3 col-xs-12">
+			        <div class="card-3 card" id="system_call">
 
-				        </div><!-- end card -->
-				    </div><!-- end column -->
+			            <div class="trend_indicator">
+			                <div class="trend_arrow"></div>
+			                <span></span>
+			            </div>
+			            <h1 class="title">{{__('widgets.system_call')}}%</h1>
+			            <h4 class="data total mt30"></h4>
 
-				    <div class="col-sm-3 col-xs-12">
-				        <div class="card-3 card" id="system_call">
+			        </div><!-- end card -->
+			    </div><!-- end column -->
 
-				            <div class="trend_indicator">
-				                <div class="trend_arrow"></div>
-				                <span></span>
-				            </div>
-				            <h1 class="title">{{__('widgets.system_call')}}%</h1>
-				            <h4 class="data total mt30"></h4>
+			    <div class="col-sm-3 col-xs-12">
+			        <div class="card-3 card" id="total_minutes">
 
-				        </div><!-- end card -->
-				    </div><!-- end column -->
+			            <div class="trend_indicator">
+			                <div class="trend_arrow"></div>
+			                <span></span>
+			            </div>
+			            <h1 class="title">{{__('widgets.total_minutes')}}</h1>
+			            <h4 class="data total mt30"></h4>
 
-				    <div class="col-sm-3 col-xs-12">
-				        <div class="card-3 card" id="total_minutes">
+			        </div><!-- end card -->
+			    </div><!-- end column -->
 
-				            <div class="trend_indicator">
-				                <div class="trend_arrow"></div>
-				                <span></span>
-				            </div>
-				            <h1 class="title">{{__('widgets.total_minutes')}}</h1>
-				            <h4 class="data total mt30"></h4>
+			    <div class="col-sm-3 col-xs-12">
+			        <div class="card-3 card" id="total_calls">
 
-				        </div><!-- end card -->
-				    </div><!-- end column -->
+			            <div class="trend_indicator">
+			                <div class="trend_arrow"></div>
+			                <span></span>
+			            </div>
+			            <h1 class="title">{{__('widgets.total_calls')}}</h1>
+			            <h4 class="data total mt30"></h4>
 
-				    <div class="col-sm-3 col-xs-12">
-				        <div class="card-3 card" id="total_calls">
+			        </div><!-- end card -->
+			    </div><!-- end column -->
+			</div>
 
-				            <div class="trend_indicator">
-				                <div class="trend_arrow"></div>
-				                <span></span>
-				            </div>
-				            <h1 class="title">{{__('widgets.total_calls')}}</h1>
-				            <h4 class="data total mt30"></h4>
-
-				        </div><!-- end card -->
-				    </div><!-- end column -->
-				</div>
-
-				<div class="row">
-					<div class="col-sm-3 col-xs-6">
-					    <div class="card-3 card blue" id="callstatus_by_minutes">
-					        <h1 class="title">{{__('widgets.callstatus_by_minutes')}}</h1>
-					        <h4 class="data count total"></h4>
-					    </div><!-- end card -->
-					</div><!-- end column -->
-				</div>
+			<div class="row">
+				<div class="col-sm-3 col-xs-6">
+				    <div class="card-3 card blue" id="callstatus_by_minutes">
+				        <h1 class="title">{{__('widgets.callstatus_by_minutes')}}</h1>
+				        <h4 class="data count total"></h4>
+				    </div><!-- end card -->
+				</div><!-- end column -->
 			</div>
 		</div>
 	</div>
-</div>
 
+@include('shared.datepicker')
 @include('shared.reportmodal')
+@endsection
