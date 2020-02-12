d7d3421d3debfeb92f22374b72f11a3a6581ed33
Thomas Grauer <tgrauer1@me.com>
2020-02-12 15:48:57 -0500

    Duration dash

--- public/js/admindurationdash.js
+++ public/js/admindurationdash.js
@@ -40,7 +40,7 @@ Chart.pluginService.register({
     }
 });
 
-var DurationDash = {
+var Dashboard = {
 
 	chartColors: {
 	    red: 'rgb(255,67,77)',
@@ -59,13 +59,13 @@ var DurationDash = {
 	init:function(){
 
 		/// dashboard widgets
-		$.when(this.call_volume(this.datefilter)).done(function () {
+		$.when(this.call_volume(this.datefilter, this.chartColors)).done(function () {
 		    $('.preloader').fadeOut('slow');
 		    Master.check_reload();
 		});
 	},
 
-	call_volume:function(datefilter){
+	call_volume:function(datefilter, chartColors){
 		var campaign = $('.filter_campaign li ').text();
 
 		$.ajaxSetup({
@@ -85,11 +85,216 @@ var DurationDash = {
 
 			success: function (response) {
 				console.log(response);
+
+				$('.filter_time_camp_dets p .selected_campaign').html(response.call_volume.details[0]);
+                $('.filter_time_camp_dets p .selected_datetime').html(response.call_volume.details[1]);
+				$('#connect .total').html(response.call_volume.connect_pct);
+				$('#system_call .total').html(response.call_volume.system_pct);
+				$('#total_minutes .total').html(Master.convertSecsToHrsMinsSecs(response.call_volume.total_minutes));
+				$('#total_calls .total').html(response.call_volume.total_calls);
+
+				////////////////////////////////////////////////////////////
+				////    MINUTES BY CALLSTATUS DOUGNUT GRAPH
+				///////////////////////////////////////////////////////////
+
+				if (window.minutes_by_callstatus_chart != undefined) {
+				    window.minutes_by_callstatus_chart.destroy();
+				}
+
+				var response_length = response.call_volume.callstatuses.length;
+				const callstatuses_obj = response.call_volume.callstatuses
+                const callstatuses_obj_keys = Object.getOwnPropertyNames(callstatuses_obj);
+                var chart_colors_array = Master.return_chart_colors_hash(callstatuses_obj_keys);
+
+				let callstatuses = [];
+
+				if (callstatuses_obj_keys.length) {
+				    for (let i = 0; i < callstatuses_obj_keys.length; i++) {
+				        callstatuses.push(Object.values(callstatuses_obj)[i]['Minutes']);
+				    }
+				}
+
+				var callstatus_by_minutes_data = {
+					datasets: [{
+						data:callstatuses,
+						backgroundColor: chart_colors_array,
+					}],
+				    labels: callstatuses_obj_keys,
+				    elements: {
+				        center: {
+				            color: '#203047',
+				            fontStyle: 'Segoeui',
+				            sidePadding: 15
+				        }
+				    },
+				};
+
+				var callstatus_by_minutes_options = {
+				    responsive: true,
+				    legend: {
+				        display: false
+				    },
+				    tooltips: {
+				        enabled: true,
+				    }
+				}
+
+				var ctx = document.getElementById('callstatus_by_minutes_graph').getContext('2d');
+
+				window.minutes_by_callstatus_chart = new Chart(ctx, {
+				    type: 'doughnut',
+				    data: callstatus_by_minutes_data,
+				    options: callstatus_by_minutes_options
+				});
+
+				////////////////////////////////////////////////////////////
+				////    CALLS & MINUTES PER DAY BAR GRAPH
+				///////////////////////////////////////////////////////////
+
+				if (window.calls_minutes_per_day_chart != undefined) {
+				    window.calls_minutes_per_day_chart.destroy();
+				}
+
+				const calls_minutes_per_day_obj = response.call_volume.dates
+                const calls_minutes_per_day_obj_keys = Object.getOwnPropertyNames(calls_minutes_per_day_obj);
+                var chart_colors_array = Master.return_chart_colors_hash(calls_minutes_per_day_obj_keys);
+                console.log(calls_minutes_per_day_obj_keys.length);
+
+				let call_minutes = [];
+				let call_count = [];
+
+				if (calls_minutes_per_day_obj_keys.length) {
+				    for (let i = 0; i < calls_minutes_per_day_obj_keys.length; i++) {
+				    	console.log(Object.values(calls_minutes_per_day_obj)[i]['Minutes']);
+				        call_minutes.push(Object.values(calls_minutes_per_day_obj)[i]['Minutes']);
+				        call_count.push(Object.values(calls_minutes_per_day_obj)[i]['Count']);
+				    }
+				}
+
+				console.log(call_minutes);
+				console.log(call_count);
+
+				var calls_minutes_per_day_data = {
+                 	labels: calls_minutes_per_day_obj_keys,
+                    datasets: [
+                      {
+                        yAxisID: 'A',
+                        label: Lang.get('js_msgs.call_time'),
+                        backgroundColor: chartColors.green,
+                        data: call_minutes
+                      },
+                      {
+                        yAxisID: 'B',
+                        label: Lang.get('js_msgs.call_count'),
+                        backgroundColor: chartColors.orange,
+                        fillOpacity: .5, 
+                        data: call_count
+                      }
+                    ]
+                };
+
+                var calls_minutes_per_day_options={
+                    responsive: true,
+                    maintainAspectRatio:false,
+                    legend: {  
+                        position: 'bottom',
+                        labels: {
+                            boxWidth: 12,
+                            fontColor: Master.tick_color,
+                        } 
+                    },
+                    scales: {
+                        xAxes: [{
+                            ticks: {
+                                fontColor: Master.tick_color,
+                            },
+                            gridLines: {
+                                color: Master.gridline_color,
+                            },
+                        }],
+                        yAxes: [
+
+                            {
+                                gridLines: {
+                                    color: Master.gridline_color,
+                                },
+                                id:'A',
+                                type: 'linear',
+                                position:'left',
+                                scalePositionLeft: true,
+                                scaleLabel: {
+                                    fontColor: Master.tick_color,
+                                    display: true,
+                                    labelString: Lang.get('js_msgs.minutes')
+                                },
+                                ticks: {
+                                    beginAtZero: true,
+                                    // callback: function(value, index, values) {
+                                    //     if(show_decimal){
+                                    //         return Math.round((parseInt(value) /60) * 10) / 10;
+                                    //     }else{
+                                    //         return Math.round(parseInt(value) / 60);
+                                    //     }
+                                    // }
+                                }
+                            },
+                            {
+                                id:'B',
+                                type: 'linear',
+                                position:'right',
+                                scalePositionLeft: false,
+                                scaleLabel: {
+                                    fontColor: Master.tick_color,
+                                    display: true,
+                                    labelString: Lang.get('js_msgs.call_count')
+                                },
+                                ticks: {
+                                    fontColor: Master.tick_color,
+                                    beginAtZero: true,
+                                }
+                            }
+
+                        ]
+                    },
+                    tooltips: {
+                        enabled: true,
+                        mode: 'single',
+                        callbacks: {
+                            label: function(tooltipItems, data) { 
+                                if (tooltipItems.datasetIndex === 0) {
+                                    return Master.convertSecsToHrsMinsSecs(tooltipItems.yLabel);
+                                }else{
+                                    return tooltipItems.yLabel;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                var ctx = document.getElementById('calls_minutes_per_day_graph').getContext('2d');
+
+                window.rep_talktime_chart = new Chart(ctx, {
+                    type: 'bar',
+                    data: calls_minutes_per_day_data,
+                    options: calls_minutes_per_day_options
+                });
 			}
 		});
+	},
+
+	refresh: function (datefilter, campaign) {
+
+	    $.when(
+	        this.call_volume(datefilter, this.chartColors),
+	        ).done(function () {
+	            $('.preloader').fadeOut('slow');
+	            // Dashboard.resizeCardTableDivs();
+	            Master.check_reload();
+	        });
+
 	},
 }
 
 $(document).ready(function(){
-	DurationDash.init();
+	Dashboard.init();
 });
--- resources/views/dashboards/admindurationdash.blade.php
+++ resources/views/dashboards/admindurationdash.blade.php
@@ -1,82 +1,86 @@
-@extends('layouts.dash')
 
-@section('title', __('general.admin_duration'))
-
-@section('content')
-
-<div class="preloader"></div>
-<input type="hidden" value="{{ $dateFilter }}" id="datefilter" name="datefilter">
-
-@includeWhen(!$isApi, 'shared.navbar')
-
-<div class="container-fluid bg">
-    <div class="container mt50">
-        @include('shared.filters')
-        <div class="row">
-			    <div class="col-sm-3 col-xs-12">
-			        <div class="card-3 card" id="connect">
-
-			            <div class="trend_indicator">
-			                <div class="trend_arrow"></div>
-			                <span></span>
-			            </div>
-			            <h1 class="title">{{__('widgets.connect')}} %</h1>
-			            <h4 class="data total mt30"></h4>
-
-			        </div><!-- end card -->
-			    </div><!-- end column -->
-
-			    <div class="col-sm-3 col-xs-12">
-			        <div class="card-3 card" id="system_call">
-
-			            <div class="trend_indicator">
-			                <div class="trend_arrow"></div>
-			                <span></span>
-			            </div>
-			            <h1 class="title">{{__('widgets.system_call')}}%</h1>
-			            <h4 class="data total mt30"></h4>
-
-			        </div><!-- end card -->
-			    </div><!-- end column -->
-
-			    <div class="col-sm-3 col-xs-12">
-			        <div class="card-3 card" id="total_minutes">
-
-			            <div class="trend_indicator">
-			                <div class="trend_arrow"></div>
-			                <span></span>
-			            </div>
-			            <h1 class="title">{{__('widgets.total_minutes')}}</h1>
-			            <h4 class="data total mt30"></h4>
-
-			        </div><!-- end card -->
-			    </div><!-- end column -->
-
-			    <div class="col-sm-3 col-xs-12">
-			        <div class="card-3 card" id="total_calls">
-
-			            <div class="trend_indicator">
-			                <div class="trend_arrow"></div>
-			                <span></span>
-			            </div>
-			            <h1 class="title">{{__('widgets.total_calls')}}</h1>
-			            <h4 class="data total mt30"></h4>
-
-			        </div><!-- end card -->
-			    </div><!-- end column -->
-			</div>
-
-			<div class="row">
-				<div class="col-sm-3 col-xs-6">
-				    <div class="card-3 card blue" id="callstatus_by_minutes">
-				        <h1 class="title">{{__('widgets.callstatus_by_minutes')}}</h1>
-				        <h4 class="data count total"></h4>
-				    </div><!-- end card -->
-				</div><!-- end column -->
-			</div>
+<div class="container-full mt20">
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="filter_time_camp_dets">
+                <p>
+                    <span class="selected_datetime"></span> |
+                    <span class="selected_campaign"></span>
+                </p>
+            </div>
+        </div>
     </div>
+
+    <div class="row">
+	    <div class="col-sm-3 col-xs-12">
+	        <div class="card-3 card" id="connect">
+
+	            <div class="trend_indicator">
+	                <div class="trend_arrow"></div>
+	                <span></span>
+	            </div>
+	            <h1 class="title">{{__('widgets.connect')}} %</h1>
+	            <h4 class="data total mt30"></h4>
+
+	        </div><!-- end card -->
+	    </div><!-- end column -->
+
+	    <div class="col-sm-3 col-xs-12">
+	        <div class="card-3 card" id="system_call">
+
+	            <div class="trend_indicator">
+	                <div class="trend_arrow"></div>
+	                <span></span>
+	            </div>
+	            <h1 class="title">{{__('widgets.system_call')}}%</h1>
+	            <h4 class="data total mt30"></h4>
+
+	        </div><!-- end card -->
+	    </div><!-- end column -->
+
+	    <div class="col-sm-3 col-xs-12">
+	        <div class="card-3 card" id="total_minutes">
+
+	            <div class="trend_indicator">
+	                <div class="trend_arrow"></div>
+	                <span></span>
+	            </div>
+	            <h1 class="title">{{__('widgets.total_minutes')}}</h1>
+	            <h4 class="data total mt30"></h4>
+
+	        </div><!-- end card -->
+	    </div><!-- end column -->
+
+	    <div class="col-sm-3 col-xs-12">
+	        <div class="card-3 card" id="total_calls">
+
+	            <div class="trend_indicator">
+	                <div class="trend_arrow"></div>
+	                <span></span>
+	            </div>
+	            <h1 class="title">{{__('widgets.total_calls')}}</h1>
+	            <h4 class="data total mt30"></h4>
+
+	        </div><!-- end card -->
+	    </div><!-- end column -->
+	</div>
+
+	<div class="row">
+		<div class="col-sm-3">
+		    <div class="card-3 card blue" id="callstatus_by_minutes">
+		        <h1 class="title">{{__('widgets.callstatus_by_minutes')}}</h1>
+		        <canvas id="callstatus_by_minutes_graph"></canvas>
+		    </div><!-- end card -->
+		</div><!-- end column -->
+
+		<div class="col-sm-9">
+		    <div class="card-3 card blue" id="calls_minutes_per_day">
+		        <h1 class="title">{{__('widgets.calls_minutes_per_day')}}</h1>
+		        <div style="height: 300px">
+		        	<canvas id="calls_minutes_per_day_graph"></canvas>
+		        </div>
+		    </div><!-- end card -->
+		</div><!-- end column -->
+	</div>
 </div>
 
-@include('shared.datepicker')
-
-@endsection
