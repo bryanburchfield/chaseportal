1790391a88eaa5b322d38b628066f59cbd851878
Bryan Burchfield <bburchfield@gmail.com>
2020-02-14 15:12:12 -0500

    Added automated caller id report

---
+++ app/Mail/CallerIdMail.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class CallerIdMail extends Mailable implements ShouldQueue
+{
+    use Queueable, SerializesModels;
+
+    public $data;
+    public $pdf;
+
+    public function __construct($data = [])
+    {
+        $this->data = $data;
+    }
+
+    public function build()
+    {
+        return $this
+            ->view('mail.callerid')
+            ->subject($this->data['subject'])
+            ->attachData(
+                base64_decode($this->data['pdf']),
+                'callerid.pdf',
+                [
+                    'mime' => 'application/pdf',
+                ]
+            );
+    }
+}
