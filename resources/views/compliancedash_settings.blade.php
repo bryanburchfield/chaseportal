46c01fac039799e599b9a7991a5004f84efb4c17
Thomas Grauer <tgrauer1@me.com>
2020-02-20 15:03:36 -0500

    Add compliance settings form again

--- resources/views/compliancedash_settings.blade.php
+++ resources/views/compliancedash_settings.blade.php
@@ -9,9 +9,32 @@
         <p>
             <a href="{{ action('MasterDashController@complianceDashboard') }}">Back to Dashboard</a>
         </p>
-        @foreach ($pause_codes as $code)
-            {{ $code }}
-        @endforeach
+        
+        <div class="col-sm-6 card">
+    	    <form action="#" method="post" class="form">
+    	    	<label>Code</label>
+    	    	<div class="form-group">
+    	    		<select name="" id="" class="form-control">
+    	    			<option value="">Select One</option>
+    	    			@foreach ($pause_codes as $code)
+    						<option value="{{ $code }}">{{ $code }}</option>
+    	    			@endforeach
+    	    		</select>
+    	    	</div>
+
+    	    	<div class="form-group">
+    	    		<label>Minutes Per Day</label>
+    	    		<input type="text" class="form-control mins_per_day" name="mins_per_day">
+    	    	</div>
+
+    	    	<div class="form-group">
+    	    		<label>Times Per Day</label>
+    	    		<input type="text" class="form-control times_per_day" name="times_per_day">
+    	    	</div>
+
+    	    	<input type="submit" class="btn btn-primary" value="Do Something">
+    	    </form>
+	    </div>
     </div>
 </div>
 
