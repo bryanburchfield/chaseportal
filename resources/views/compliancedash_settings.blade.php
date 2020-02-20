e0d33ed4041f5351fc6fcfc0caf0ef9a173478ab
Bryan Burchfield <bburchfield@gmail.com>
2020-02-20 14:59:44 -0500

    blade work

--- resources/views/compliancedash_settings.blade.php
+++ resources/views/compliancedash_settings.blade.php
@@ -4,18 +4,14 @@
 
 @section('content')
 
-<div class="container-fluid bg">
-    <div class="container mt50">
-        <div class="row">
-            <div class="col-sm-12">
-                <p>
-                    <a href="{{ action('MasterDashController@complianceDashboard') }}">Back to Dashboard</a>
-                </p>
-                @foreach ($pause_codes as $code)
-                    {{ $code }}
-                @endforeach
-            </div>
-        </div>
+<div class="row">
+    <div class="col-sm-12">
+        <p>
+            <a href="{{ action('MasterDashController@complianceDashboard') }}">Back to Dashboard</a>
+        </p>
+        @foreach ($pause_codes as $code)
+            {{ $code }}
+        @endforeach
     </div>
 </div>
 
