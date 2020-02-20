2515e74ca12bbc37001675a86a343f5cb6cac4ed
Thomas Grauer <tgrauer1@me.com>
2020-02-20 15:19:58 -0500

    Loop obj for settings form

--- resources/views/compliancedash_settings.blade.php
+++ resources/views/compliancedash_settings.blade.php
@@ -4,35 +4,34 @@
 
 @section('content')
 
+
 <div class="row">
     <div class="col-sm-12">
-        <p>
-            <a href="{{ action('MasterDashController@complianceDashboard') }}">Back to Dashboard</a>
-        </p>
-        
-        <div class="col-sm-6 card">
+        <p><a href="{{ action('MasterDashController@complianceDashboard') }}">Back to Dashboard</a></p>
+
+        <div class="col-sm-12 card">
     	    <form action="#" method="post" class="form">
-    	    	<label>Code</label>
-    	    	<div class="form-group">
-    	    		<select name="" id="" class="form-control">
-    	    			<option value="">Select One</option>
-    	    			@foreach ($pause_codes as $code)
-    						<option value="{{ $code }}">{{ $code }}</option>
-    	    			@endforeach
-    	    		</select>
-    	    	</div>
+    			@foreach ($pause_codes as $key => $value)
+    				<div class="form-group col-sm-4">
+    					<label>Code</label>
+    	    			<select name="" id="" class="form-control">
+    	    				<option value="">Select One</option>
+    						<option value="{{ $value['code'] }}">{{$value['code']}}</option>
+    					</select>
+					</div>
 
-    	    	<div class="form-group">
-    	    		<label>Minutes Per Day</label>
-    	    		<input type="text" class="form-control mins_per_day" name="mins_per_day">
-    	    	</div>
+					<div class="form-group col-sm-4">
+						<label>Minutes Per Day</label>
+						<input type="text" class="form-control minutes_per_day" name="minutes_per_day" value="{{$key['minutes_per_day']}}">
+					</div>
 
-    	    	<div class="form-group">
-    	    		<label>Times Per Day</label>
-    	    		<input type="text" class="form-control times_per_day" name="times_per_day">
-    	    	</div>
+					<div class="form-group col-sm-4">
+						<label>Times Per Day</label>
+						<input type="text" class="form-control times_per_day" name="times_per_day" value="{{$key['times_per_day']}}">
+					</div>
+    			@endforeach
 
-    	    	<input type="submit" class="btn btn-primary" value="Do Something">
+	    		<input type="submit" class="btn btn-primary" value="Do Something">
     	    </form>
 	    </div>
     </div>
