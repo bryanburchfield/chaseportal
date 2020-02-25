fae170707b2c9c4575df1fe4333500bba76edb75
Thomas Grauer <tgrauer1@me.com>
2020-02-25 15:53:57 -0500

    Hard code blank val, translations

--- resources/lang/es/js_msgs.php
+++ resources/lang/es/js_msgs.php
@@ -45,7 +45,7 @@ return [
 	'no_data'					=> 'Aún Sin Datos',
 	'non_callable'				=> 'No Invocable',
 	'non_callable_by_disp'		=> 'Conductores no Invocables por Disposición',
-	'none'						=> 'Nada',
+	'no_subcampaign'			=> 'Sin Subcampaña',
 	'none_selected'				=> 'Nada Seleccionado',
 	'numb_filter_attempts'		=> 'Número de Intentos para Filtrar por',
 	'ordered_by'				=> 'Ordenar por',
