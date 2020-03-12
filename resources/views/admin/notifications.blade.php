4d24edcfe875a329a13fe6d0846fdac5d11f0fc6
Bryan Burchfield <bburchfield@gmail.com>
2020-03-12 14:33:19 -0400

    only load summernote stuff on demand

--- resources/views/admin/notifications.blade.php
+++ resources/views/admin/notifications.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.master')
+@extends('layouts.master', ['summernote' => true])
 @section('title', __('widgets.admin') .' - '. __('general.notifications'))
 
 @section('content')
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -27,7 +27,9 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-multiselect/0.9.13/css/bootstrap-multiselect.css" rel="stylesheet">
     {{-- <link href="/css/jquery-ui.min.css" rel="stylesheet"> --}}
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-    <link href="{{ asset('/css/summernote.min.css') }}" rel="stylesheet">
+    @isset($summernote)
+        <link href="{{ asset('/css/summernote.min.css') }}" rel="stylesheet">
+    @endisset
     <!--[if lt IE 9]>
     <script src="/js/html5shiv.min.js"></script>
     <script src="/js/respond.min.js"></script>
@@ -66,14 +68,14 @@
     <script src="https://cdn.datatables.net/buttons/1.5.6/js/buttons.print.min.js"></script> 
     <script src="/js/color-hash.js"></script>
 
-<!-- Summernote  -->
-<script src="{{ asset('/js/summernote.min.js') }}"></script>
-
+    @isset($summernote)
+        <script src="{{ asset('/js/summernote.min.js') }}"></script>
+    @endisset
 
     @isset($jsfile)
-    @foreach($jsfile as $js)
-    <script src="/js/{{ $js }}" type="text/javascript"></script>
-    @endforeach
+        @foreach($jsfile as $js)
+            <script src="/js/{{ $js }}" type="text/javascript"></script>
+        @endforeach
     @endisset
     
     <script src="/js/master.js"></script>
@@ -99,9 +101,11 @@
             $('.btn-notifications, .close_nots_bar').on('click', function () {
                 $('#sidebar_nots').toggleClass('active');
             });
-            $('#summernote').summernote({
-                height: 200
-            });
+            @isset($summernote)
+                $('#summernote').summernote({
+                    height: 200
+                });
+            @endisset
         });
     </script>
 
