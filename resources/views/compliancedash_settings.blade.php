15c2ede1f8f0628d75de3649e4efefb140cbe0e3
Bryan Burchfield <bburchfield@gmail.com>
2020-03-13 11:44:10 -0400

    blade update

--- resources/views/compliancedash_settings.blade.php
+++ resources/views/compliancedash_settings.blade.php
@@ -4,21 +4,18 @@
 
 @section('content')
 
-
 <div class="row">
     <div class="col-sm-12">
-        <p><a href="{{ action('MasterDashController@complianceDashboard') }}">Back to Dashboard</a></p>
         <div class="card p0">
-        	<table class="table">
-        		<thead>
-        			<th>{{__('general.code')}}</th>
-        			<th>{{__('general.minutes_per_day')}}</th>
-        			<th>{{__('general.times_per_day')}}</th>
-        		</thead>
-
-        		<tbody>
-		    	    <form method="post" class="form compliance_settings">
-		    	    	@csrf
+			<form method="post" class="form compliance_settings">
+				@csrf
+				<table class="table">
+					<thead>
+						<th>{{__('general.code')}}</th>
+						<th>{{__('general.minutes_per_day')}}</th>
+						<th>{{__('general.times_per_day')}}</th>
+					</thead>
+					<tbody>
 		    			@foreach ($pause_codes as $pause_code)
 		    				<tr >
 			    				<td>
@@ -27,13 +24,11 @@
 				    	    			<input type="hidden" class="form-control code" readonly name="code[]" value="{{ $pause_code['code'] }}">
 									</div>
 			    				</td>
-
 								<td>
 									<div class="form-group">
 										<input type="text" class="form-control minutes_per_day" name="minutes_per_day[]" value="{{$pause_code['minutes_per_day']}}">
 									</div>
 								</td>
-
 								<td>
 									<div class="form-group">
 										<input type="text" class="form-control times_per_day" name="times_per_day[]"  value="{{$pause_code['times_per_day']}}">
@@ -42,12 +37,11 @@
 							</tr>
 		    			@endforeach
 
-						<input type="submit" class="btn btn-primary" value="{{__('general.save')}}" />
-						<input type="submit" class="btn btn-default btn-cancel mr10" name="cancel" value="{{__('general.cancel')}}" />
-		    	    </form>
-        		</tbody>
-        	</table>
-
+					</tbody>
+				</table>
+				<input type="submit" class="btn btn-primary mr10" value="{{__('general.save')}}" />
+				<input type="submit" class="btn btn-default btn-cancel" name="cancel" value="{{__('general.cancel')}}" />
+			</form>
 	    </div>
     </div>
 </div>
