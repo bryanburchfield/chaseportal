1261aaba94a900b21bcd46073dea8ccba8b5e5b9
Thomas Grauer <tgrauer1@me.com>
2020-03-16 13:37:47 -0400

    Change flowbuilder permissions

--- resources/views/tools/shared/topnav.blade.php
+++ resources/views/tools/shared/topnav.blade.php
@@ -1,5 +1,5 @@
 <ul class="nav nav-tabs">
-	@if ( Auth::user()->isType('admin') || Auth::user()->group_id == 777)
+	@if ( Auth::user()->isType(['admin', 'superadmin']) || Auth::user()->group_id == 777)
     <li @if ($toolpage == 'rules') class="active" @endif><a href="{{url('/tools/contactflow_builder')}}">{{__('tools.contact_flowbuilder')}}</a></li>
     @endif
     <li @if ($toolpage == 'dnc') class="active" @endif><a href="{{url('/tools/dnc_importer')}}">{{__('tools.dnc_importer')}}</a></li>
