5280e0f244875a097878a937f71a9e07d3d5c33c
Thomas Grauer <tgrauer1@me.com>
2020-03-18 15:33:07 -0400

    Campaign search

--- public/js/agentdash.js
+++ public/js/agentdash.js
@@ -53,6 +53,8 @@ var Dashboard = {
     },
 
     datefilter : document.getElementById("datefilter").value,
+    first_search: true,
+    active_camp_search: '',
 
     init:function(){
         $.when(this.get_call_volume(this.datefilter, this.chartColors), this.campaign_stats(this.datefilter, this.chartColors), this.get_total_conversions(this.datefilter)).done(function(){
@@ -75,6 +77,9 @@ var Dashboard = {
 
     eventHandlers:function(){
         $('.date_filters li a').on('click', this.filter_date);
+        $('.campaign_search').on('keyup', this.search_campaigns);
+        $('.filter_campaign').on('click', '.campaign_group', this.adjust_campaign_filters);
+        $('.select_campaign').on('click', this.filter_campaign);
     },
 
     display_error:function(div, textStatus, errorThrown){
@@ -363,7 +368,156 @@ var Dashboard = {
     title_options :{
         fontColor:'#144da1',
         fontSize:16,
-    }
+    },
+
+    search_campaigns: function () {
+        var query = $(this).val();
+
+        if (Dashboard.first_search) {
+            if ($('.filter_campaign li').hasClass('active')) {
+                Dashboard.active_camp_search = $('.filter_campaign li.active').text();
+            }
+        }
+
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
+            }
+        });
+
+        $.ajax({
+            url: '/dashboards/campaign_search',
+            type: 'POST',
+            dataType: 'json',
+            data: { query: query },
+            success: function (response) {
+
+                var is_array = Array.isArray(response.search_result);
+                var obj = response['search_result'];
+                $('.filter_campaign .checkbox').remove();
+                var campaign_searchresults = '';
+
+                if (!is_array) {
+                    var obj = Object.keys(obj).map(function (key) {
+                        return [obj[key]];
+                    });
+                }
+
+                var checked;
+
+                for (var i = 0; i < obj.length; i++) {
+                    checked = obj[i].selected;
+                    if (checked) { checked = 'checked'; } else { checked = ''; }
+                    campaign_searchresults += '<div class="checkbox"><label class="campaign_label stop-propagation"><input class="campaign_group" required type="checkbox" ' + checked + ' value="' + obj[i].value + '" name="campaigns"><span>' + obj[i].name + '</span></label></div>';
+                }
+
+                Dashboard.first_search = false;
+
+                $('.filter_campaign').append(campaign_searchresults);
+            }
+        });
+    },
+
+    adjust_campaign_filters: function () {
+
+        // Get amount of selected checkboxes
+        var checked = [];
+        $('.campaign_label input:checked').each(function () {
+            checked.push($(this).attr('name'));
+        });
+
+        /// check if target is NOT All Camps
+        if ($(this).val() != '') {
+            // See if others are checked
+            if (checked.length) {
+                // check if All Camps is checked
+                if ($('.filter_campaign .campaign_group').eq(0).is(':checked')) {
+                    // uncheck all camps because others are being selected
+                    $('.filter_campaign .campaign_group').eq(0).removeAttr('checked');
+                }
+            }
+        } else { /// ALL camps is being checked
+            // check if All Camps was already checked
+            if ($('.filter_campaign .campaign_group').eq(0).is(':checked')) {
+                $('.filter_campaign .campaign_group').removeAttr('checked'); /// uncheck all other camps
+                $('.filter_campaign .campaign_group').eq(0).prop('checked', true); // recheck all camps
+            }
+
+            if (!checked.length) { // if nothing is selected reselect All Camps because something has to be checked
+                $('.filter_campaign .campaign_group').eq(0).prop('checked', true);
+            }
+        }
+    },
+
+    // ran when submit is clicked in the interaction menu
+    filter_campaign: function () {
+
+        $('.preloader').show();
+
+        datefilter = $('#datefilter').val();
+        var checked = $(".campaign_group:checkbox:checked").length;
+        $('.alert').remove();
+        $('.campaign_search').val('');
+
+        if (checked) {
+            $('.filter_campaign').parent().removeClass('open');
+            $('.filter_campaign').prev('.dropdown-toggle').attr('aria-expanded', false);
+            var campaigns = [];
+            $('.filter_campaign .checkbox label input[name="campaigns"]:checked').each(function () {
+                campaigns.push($(this).val());
+            });
+        }
+
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
+            }
+        });
+
+        $.ajax({
+            url: '/dashboards/update_filters',
+            type: 'POST',
+            dataType: 'json',
+            data: { campaign: campaigns },
+            success: function (response) {
+                Dashboard.set_campaigns(response);
+            }
+        });
+    },
+
+    // ran after submit is clicked in the interaction menu, after filter_campaign()
+    set_campaigns: function (response) {
+        var campaigns = [];
+        $('.filter_campaign .checkbox label input[name="campaigns"]:checked').each(function () {
+            campaigns.push($(this).val());
+            //// if total is selected, uncheck all checkboxes
+            if ($(this).val() == '') {
+                $('.filter_campaign .checkbox label input[name="campaigns"]:checkbox').removeAttr('checked');
+            }
+        });
+
+        var is_array = Array.isArray(response.campaigns);
+        var obj = response['campaigns'];
+        $('.filter_campaign .checkbox').remove();
+        var campaign_searchresults = '';
+
+        if (!is_array) {
+            var obj = Object.keys(obj).map(function (key) {
+                return [obj[key]];
+            });
+        }
+        var checked;
+
+        for (var i = 0; i < obj.length; i++) {
+            checked = obj[i].selected;
+            if (checked) { checked = 'checked'; } else { checked = ''; }
+            campaign_searchresults += '<div class="checkbox"><label class="campaign_label stop-propagation"><input class="campaign_group" required type="checkbox" ' + checked + ' value="' + obj[i].value + '" name="campaigns"><span>' + obj[i].name + '</span></label></div>';
+        }
+
+        $('.filter_campaign').append(campaign_searchresults);
+
+        Dashboard.refresh(datefilter);
+    },
 }
 
 $(document).ready(function(){
