d626ba6f1a1b09de7eaee52f4c2d2b1aa249ff07
Bryan Burchfield <bburchfield@gmail.com>
2020-04-09 10:56:30 -0400

    working on filters

--- app/Http/Controllers/PlaybookFilterController.php
+++ app/Http/Controllers/PlaybookFilterController.php
@@ -32,7 +32,7 @@ class PlaybookFilterController extends Controller
             'jsfile' => ['playbook_filters.js'],
             'group_id' => Auth::user()->group_id,
             'campaigns' => $this->getAllCampaigns(),
-            'operators' => $this->getOperators(),
+            'operators' => PlaybookFilter::getOperators(),
             'playbook_filters' => $this->getPlaybookFilters(),
         ];
 
@@ -52,115 +52,7 @@ class PlaybookFilterController extends Controller
     }
 
     /**
-     * Return list of operators for filters
-     * 
-     * @param bool $detail 
-     * @return array 
-     */
-    private function getOperators($detail = false)
-    {
-        $mathops_detail = [
-            '=' => [
-                'description' => trans('tools.equals'),
-                'allow_nulls' => false,
-                'value_type' => 'string',
-            ],
-            '!=' => [
-                'description' => trans('tools.not_equals'),
-                'allow_nulls' => false,
-                'value_type' => 'string',
-            ],
-            '<' => [
-                'description' => trans('tools.less_than'),
-                'allow_nulls' => false,
-                'value_type' => 'string',
-            ],
-            '>' => [
-                'description' => trans('tools.greater_than'),
-                'allow_nulls' => false,
-                'value_type' => 'string',
-            ],
-            '<=' => [
-                'description' => trans('tools.less_than_or_equals'),
-                'allow_nulls' => false,
-                'value_type' => 'string',
-            ],
-            '>=' => [
-                'description' => trans('tools.greater_than_or_equals'),
-                'allow_nulls' => false,
-                'value_type' => 'string',
-            ],
-            'blank' => [
-                'description' => trans('tools.is_blank'),
-                'allow_nulls' => true,
-                'value_type' => null,
-            ],
-            'not_blank' => [
-                'description' => trans('tools.is_not_blank'),
-                'allow_nulls' => true,
-                'value_type' => null,
-            ],
-        ];
-
-        $dateops_detail = [
-            'days_ago' => [
-                'description' => trans('tools.days_ago'),
-                'allow_nulls' => false,
-                'value_type' => 'integer',
-            ],
-            'days_from_now' => [
-                'description' => trans('tools.days_from_now'),
-                'allow_nulls' => false,
-                'value_type' => 'integer',
-            ],
-            '<_days_ago' => [
-                'description' => trans('tools.less_than_days_ago'),
-                'allow_nulls' => false,
-                'value_type' => 'integer',
-            ],
-            '>_days_ago' => [
-                'description' => trans('tools.greater_than_days_ago'),
-                'allow_nulls' => false,
-                'value_type' => 'integer',
-            ],
-            '<_days_from_now' => [
-                'description' => trans('tools.less_than_days_from_now'),
-                'allow_nulls' => false,
-                'value_type' => 'integer',
-            ],
-            '>_days_from_now' => [
-                'description' => trans('tools.greater_than_days_from_now'),
-                'allow_nulls' => false,
-                'value_type' => 'integer',
-            ],
-        ];
-
-        // there's a better way to do this
-        if ($detail) {
-            $mathops = $mathops_detail;
-            $dateops = $dateops_detail;
-        } else {
-            $mathops = [];
-            $dateops = [];
-            foreach ($mathops_detail as $key => $array) {
-                $mathops[$key] = $array['description'];
-            }
-            foreach ($dateops_detail as $key => $array) {
-                $dateops[$key] = $array['description'];
-            }
-        }
-
-        return [
-            'integer' => $mathops,
-            'string' => $mathops,
-            'date' => array_merge($mathops, $dateops),
-            'text' => $mathops,
-            'phone' => $mathops,
-        ];
-    }
-
-    /**
-     * Return all string fields of the Custom Table tied to a campaign
+     * Return fields of the Custom Table tied to a campaign
      * 
      * @param Request $request 
      * @return array|mixed 
@@ -173,14 +65,7 @@ class PlaybookFilterController extends Controller
 
         $fields = [];
         foreach ($campaign->advancedTable->advancedTableFields as $field) {
-            // only return 2:string, 4:text, 5:phone
-            if (
-                $field->fieldType->id == 2 ||
-                $field->fieldType->id == 4 ||
-                $field->fieldType->id == 5
-            ) {
-                $fields[$field->FieldName] = $field->fieldType->Type;
-            }
+            $fields[$field->FieldName] = $field->fieldType->Type;
         }
 
         return $fields;
@@ -190,17 +75,23 @@ class PlaybookFilterController extends Controller
     {
         Log::debug('Add');
         Log::debug($request);
+
+        return ['status' => 'success'];
     }
 
     public function updateFilter(Request $request)
     {
         Log::debug('Update');
         Log::debug($request);
+
+        return ['status' => 'success'];
     }
 
     public function deleteFilter(Request $request)
     {
         Log::debug('Delete');
         Log::debug($request);
+
+        return ['status' => 'success'];
     }
 }
--- app/Models/PlaybookFilter.php
+++ app/Models/PlaybookFilter.php
@@ -14,4 +14,112 @@ class PlaybookFilter extends Model
         'operator',
         'value',
     ];
+
+    /**
+     * Return list of operators for filters
+     * 
+     * @param bool $detail 
+     * @return array 
+     */
+    public static function getOperators($detail = false)
+    {
+        $mathops_detail = [
+            '=' => [
+                'description' => trans('tools.equals'),
+                'allow_nulls' => false,
+                'value_type' => 'string',
+            ],
+            '!=' => [
+                'description' => trans('tools.not_equals'),
+                'allow_nulls' => false,
+                'value_type' => 'string',
+            ],
+            '<' => [
+                'description' => trans('tools.less_than'),
+                'allow_nulls' => false,
+                'value_type' => 'string',
+            ],
+            '>' => [
+                'description' => trans('tools.greater_than'),
+                'allow_nulls' => false,
+                'value_type' => 'string',
+            ],
+            '<=' => [
+                'description' => trans('tools.less_than_or_equals'),
+                'allow_nulls' => false,
+                'value_type' => 'string',
+            ],
+            '>=' => [
+                'description' => trans('tools.greater_than_or_equals'),
+                'allow_nulls' => false,
+                'value_type' => 'string',
+            ],
+            'blank' => [
+                'description' => trans('tools.is_blank'),
+                'allow_nulls' => true,
+                'value_type' => null,
+            ],
+            'not_blank' => [
+                'description' => trans('tools.is_not_blank'),
+                'allow_nulls' => true,
+                'value_type' => null,
+            ],
+        ];
+
+        $dateops_detail = [
+            'days_ago' => [
+                'description' => trans('tools.days_ago'),
+                'allow_nulls' => false,
+                'value_type' => 'integer',
+            ],
+            'days_from_now' => [
+                'description' => trans('tools.days_from_now'),
+                'allow_nulls' => false,
+                'value_type' => 'integer',
+            ],
+            '<_days_ago' => [
+                'description' => trans('tools.less_than_days_ago'),
+                'allow_nulls' => false,
+                'value_type' => 'integer',
+            ],
+            '>_days_ago' => [
+                'description' => trans('tools.greater_than_days_ago'),
+                'allow_nulls' => false,
+                'value_type' => 'integer',
+            ],
+            '<_days_from_now' => [
+                'description' => trans('tools.less_than_days_from_now'),
+                'allow_nulls' => false,
+                'value_type' => 'integer',
+            ],
+            '>_days_from_now' => [
+                'description' => trans('tools.greater_than_days_from_now'),
+                'allow_nulls' => false,
+                'value_type' => 'integer',
+            ],
+        ];
+
+        // there's a better way to do this
+        if ($detail) {
+            $mathops = $mathops_detail;
+            $dateops = $dateops_detail;
+        } else {
+            $mathops = [];
+            $dateops = [];
+            foreach ($mathops_detail as $key => $array) {
+                $mathops[$key] = $array['description'];
+            }
+            foreach ($dateops_detail as $key => $array) {
+                $dateops[$key] = $array['description'];
+            }
+        }
+
+        return [
+            'integer' => $mathops,
+            'string' => $mathops,
+            'date' => array_merge($mathops, $dateops),
+            'text' => $mathops,
+            'phone' => $mathops,
+        ];
+    }
 }
