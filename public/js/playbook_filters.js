1dcee8ccfdc1a326707f0e2a8e08ef6106c374c8
Thomas Grauer <tgrauer1@me.com>
2020-04-10 15:00:27 -0400

    Filter progress

--- public/js/playbook_filters.js
+++ public/js/playbook_filters.js
@@ -4,6 +4,7 @@ var Playbook_Filters = {
 		// $("#addFilterModal").on('show.bs.modal', this.get_fields);
 		$('.filter_campaigns').on('change', this.get_fields);
 		$('.add_filter').on('change', '.filter_fields', this.get_operators);
+		$('.add_filter').on('submit', this.add_filter);
 	},
 
 	get_fields: function () {
@@ -21,7 +22,7 @@ var Playbook_Filters = {
 			type: 'POST',
 			data: {campaign:campaign},
 			success: function (response) {
-				var filter_fields = [];
+				var filter_fields = '<option value="">Select One</option>';
 				for(var i=0;i<Object.entries(response).length;i++){
 			    	filter_fields += '<option data-type="'+Object.entries(response)[i][1]+'" value="'+Object.entries(response)[i][0]+'">'+Object.entries(response)[i][0]+'</option>';
 			    }
@@ -48,12 +49,34 @@ var Playbook_Filters = {
 				type:type
 			},
 			success: function (response) {
-				var operators = [];
+				var operators;
 				for(var i=0;i<Object.entries(response).length;i++){
 					operators += '<option value="'+Object.entries(response)[i][0]+'">'+Object.entries(response)[i][1]+'</option>';					
 				}
 
 				$('.filter_operators').append(operators);
+			}
+		});
+	},
+
+	add_filter:function(e){
+		e.preventDefault();
+		var form = $(this).serialize();
+
+		$.ajaxSetup({
+			headers: {
+				'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
+			}
+		});
+
+		$.ajax({
+			url: '/tools/playbook/filters',
+			type: 'POST',
+			data: {
+				form:form
+			},
+			success: function (response) {
+				console.log(response);
 			}
 		});
 	}
