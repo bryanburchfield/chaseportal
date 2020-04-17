be5418dd498b10f7729337d1f35f5a11e5f4cd48
Thomas Grauer <tgrauer1@me.com>
2020-04-17 11:55:28 -0400

    Info styling

--- resources/views/reports/info.blade.php
+++ resources/views/reports/info.blade.php
@@ -6,21 +6,22 @@
     <a class="btn btn-primary btn_flt_rgt" href="{{ action('ReportController@index', ['report' => $report]) }}">{{__('tools.back')}}</a>
     <h3 class="heading">{{__('reports.' . $report)}}</h3>
 
-	<div class="report_filters card col-sm-12">
+	<div class="report_filters col-sm-8 mt20 p0">
 
         @for ($i = 1; $i <= $paragraphs; $i++)
             <p class="mb10">{{__('report_info.' . $report . '_' . $i)}}</p>
         @endfor
-
-        @if (count($columns))
-            <h5 class="mb10">{{__('report_info.columns')}}:</h5>
-            @foreach ($columns as $column)
-                <p class="mb5">
-                    <b>{{__($column)}}</b> : {{__('report_info.' . Str::after($column, '.'))}}
-                </p>
-            @endforeach
-        @endif
-
 	</div><!-- end report_filters -->
+
+	<div class="col-sm-4 card report_info_columns">
+		@if (count($columns))
+		    <h4 class="mb10"><b>{{__('report_info.columns')}}</b></h4>
+		    @foreach ($columns as $column)
+		        <p class="mb5">
+		            <b>{{__($column)}}</b> : {{__('report_info.' . Str::after($column, '.'))}}
+		        </p>
+		    @endforeach
+		@endif
+	</div>
 
 @endsection
