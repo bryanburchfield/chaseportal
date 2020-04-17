b2f4038a3d8770fc8852afcfe499b3b67e3047fb
Bryan Burchfield <bburchfield@gmail.com>
2020-04-17 08:42:00 -0400

    fixed fillable field list

--- app/Models/PlaybookEmailAction.php
+++ app/Models/PlaybookEmailAction.php
@@ -9,10 +9,11 @@ class PlaybookEmailAction extends Model
     protected $fillable = [
         'playbook_action_id',
         'email_service_provider_id',
-        'template_id',
-        'email_field',
-        'days_between_emails',
-        'emails_per_lead',
+        'subject',
         'from',
+        'email_field',
+        'template_id',
+        'emails_per_lead',
+        'days_between_emails',
     ];
 }
