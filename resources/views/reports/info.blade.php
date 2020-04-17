5a1a08cfa4915eb7a5f1c7e9cc7a27d7f5c69f75
Thomas Grauer <tgrauer1@me.com>
2020-04-17 11:19:40 -0400

    Blade styling

--- resources/views/reports/info.blade.php
+++ resources/views/reports/info.blade.php
@@ -2,14 +2,14 @@
 @section('title', __('general.reports'))
 
 @section('content')
-	<h3 class="heading">{{__('reports.' . $report)}}</h3>
 
     <a class="btn btn-primary btn_flt_rgt" href="{{ action('ReportController@index', ['report' => $report]) }}">{{__('tools.back')}}</a>
+    <h3 class="heading">{{__('reports.' . $report)}}</h3>
 
 	<div class="report_filters card col-sm-12">
 
         @foreach ($report_info as $paragraph)
-            <p>{{$paragraph}}</p>
+            <p class="mb10">{{$paragraph}}</p>
         @endforeach
 
 	</div><!-- end report_filters -->
