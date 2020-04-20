9728d2f36053a56a0df731a7b9936e5ffaf80260
Thomas Grauer <tgrauer1@me.com>
2020-04-20 08:36:29 -0400

    Switch info columns

--- resources/views/reports/info.blade.php
+++ resources/views/reports/info.blade.php
@@ -6,13 +6,6 @@
     <a class="btn btn-primary btn_flt_rgt" href="{{ action('ReportController@index', ['report' => $report]) }}">{{__('tools.back')}}</a>
     <h3 class="heading">{{__('reports.' . $report)}}</h3>
 
-	<div class="report_desc col-sm-8 mt20 p0">
-
-        @for ($i = 1; $i <= $paragraphs; $i++)
-            <p class="mb10">{{__('report_info.' . $report . '_' . $i)}}</p>
-        @endfor
-	</div><!-- end report_filters -->
-
 	<div class="col-sm-4 card report_info_columns">
 		@if (count($columns))
 		    <h4 class="mb10"><b>{{__('report_info.columns')}}</b></h4>
@@ -23,5 +16,12 @@
 		    @endforeach
 		@endif
 	</div>
+
+		<div class="report_desc col-sm-8 mt20 ">
+
+	        @for ($i = 1; $i <= $paragraphs; $i++)
+	            <p class="mb10">{{__('report_info.' . $report . '_' . $i)}}</p>
+	        @endfor
+		</div><!-- end report_filters -->
 
 @endsection
