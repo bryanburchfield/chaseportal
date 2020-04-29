7a6c27e7a8e79731aa4966ea95de3bda16d02ac9
Bryan Burchfield <bburchfield@gmail.com>
2020-04-29 09:29:09 -0400

    playbook functions

--- app/Http/Requests/ValidPlaybook.php
+++ app/Http/Requests/ValidPlaybook.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Requests;
 
+use App\Models\ContactsPlaybook;
+use Illuminate\Validation\Rule;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
 
 class ValidPlaybook extends FormRequest
 {
@@ -17,14 +20,42 @@ class ValidPlaybook extends FormRequest
     }
 
     /**
+     * Prepare the data for validation.
+     *
+     * @return void
+     */
+    protected function prepareForValidation()
+    {
+        // if id not passed (adding), insert id=0
+        // otherwise, check that filter belongs to user's group_id, 404 if not
+        if ($this->filled('id')) {
+            $contacts_playbook = ContactsPlaybook::where('id', $this->id)
+                ->where('group_id', Auth::user()->group_id)
+                ->firstOrFail();
+        } else {
+            $this->merge(['id' => 0]);
+        }
+    }
+
+    /**
      * Get the validation rules that apply to the request.
      *
      * @return array
      */
     public function rules()
     {
+        $group_id = Auth::user()->group_id;
+        $id = $this->id;
+
         return [
-            'name' => 'required',
+            'name' => [
+                'required',
+                Rule::unique('contacts_playbooks')->where(function ($query) use ($group_id, $id) {
+                    return $query
+                        ->where('group_id', $group_id)
+                        ->where('id', '!=', $id);
+                }),
+            ],
             'campaign' => 'required',
             'subcampaign' => 'nullable',
         ];
