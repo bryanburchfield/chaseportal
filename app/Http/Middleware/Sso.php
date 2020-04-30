cb0c01e18a5bb6bcb9862655c297f50790c722b3
Bryan Burchfield <bburchfield@gmail.com>
2020-04-30 09:59:44 -0400

    sso user creation

--- app/Http/Middleware/Sso.php
+++ app/Http/Middleware/Sso.php
@@ -21,29 +21,34 @@ class Sso
         if (Auth::guest()) {
 
             // call API to get stuff
-
-
-
-            // find or create 'fake' user
-            // username will be user_group_a  or _r  for admin or rep
-            // Case sensitive!!  Will probably have to create a hash for it
-
             if ($request->query('token') == '12345') {
-                $user = User::find(38);
-            } else {
-                $user = User::find(-1);
+                $sso_user = [
+                    'name' => 'bryan',
+                    'type' => 'rep',
+                    'group_id' => '777',
+                    'reporting_db' => 'PowerV2_Reporting_Dialer-07',
+                    'timezone' => 'Eastern Standard Time',
+                ];
             }
 
             // Abort if not authorized
+            if (empty($sso_user['name'])) {
+                abort(403, 'Unauthorized');
+            }
+
+            // Find or create the SSO user
+            $user = User::getSsoUser($sso_user);
+
+            // Abort if this blew up somehow
             if (!$user) {
                 abort(403, 'Unauthorized');
             }
 
-            // Login as that user
-            Auth::login($user);
-
             // set 'sso' on session
             session(['isSso' => 1]);
+
+            // Login as that user
+            Auth::login($user);
         }
 
         return $next($request);
