4f4b396f321f1ff9b12a158cc2e07947642ee682
Thomas Grauer <tgrauer1@me.com>
2020-05-12 14:30:08 -0400

    style blades

--- resources/views/tools/playbook/optout.blade.php
+++ resources/views/tools/playbook/optout.blade.php
@@ -1,3 +1,3 @@
 <p>
-    <a href="{{$optouturl}}">Unsubscribe</a>
+    <a style="color:#48B5D8; font-weight:600;text-decoration:none;background:#fff;padding:3px 5px;" href="{{$optouturl}}">Unsubscribe</a>
 </p>
--- resources/views/tools/playbook/unsubscribed.blade.php
+++ resources/views/tools/playbook/unsubscribed.blade.php
@@ -1,1 +1,14 @@
-You will no longer recieve emails.
+@extends('layouts.master')
+@section('title', __('tools.tools'))
+
+@section('content')
+	
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-6 col-sm-offset-3 mt100">
+				<div class="alert alert-info fz15 tac">You will no longer recieve emails.</div>
+			</div>
+		</div>
+	</div>
+
+@endsection
