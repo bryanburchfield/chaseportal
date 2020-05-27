05fcb0ffa314eff049067190d9cce3eee91be2e4
Thomas Grauer <tgrauer1@me.com>
2020-05-27 12:04:58 -0400

    Settings blade, translations

--- resources/views/compliancedash_settings.blade.php
+++ resources/views/compliancedash_settings.blade.php
@@ -6,7 +6,7 @@
 
 <div class="row">
     <div class="col-sm-12">
-        <div class="card p0">
+        <div class="card p0 mt30">
 			<form method="post" class="form compliance_settings">
 				@csrf
 				<table class="table">
@@ -22,20 +22,20 @@
 					</thead>
 					<tbody>
 		    			@foreach ($pause_codes as $pause_code)
-		    				<tr >
+		    				<tr class="p10">
 			    				<td>
-				    				<div class="form-group">
+				    				<div class="form-group mb0">
 				    					<h5><b>{{$pause_code['code']}}</b></h5>
 				    	    			<input type="hidden" class="form-control code" readonly name="code[]" value="{{ $pause_code['code'] }}">
 									</div>
 			    				</td>
 								<td>
-									<div class="form-group">
+									<div class="form-group mb0">
 										<input type="text" class="form-control minutes_per_day" name="minutes_per_day[]" value="{{$pause_code['minutes_per_day']}}">
 									</div>
 								</td>
 								<td>
-									<div class="form-group">
+									<div class="form-group mb0">
 										<input type="text" class="form-control times_per_day" name="times_per_day[]"  value="{{$pause_code['times_per_day']}}">
 									</div>
 								</td>
@@ -44,8 +44,9 @@
 
 					</tbody>
 				</table>
-				<input type="submit" class="btn btn-primary mr10" value="{{__('general.save')}}" />
-				<input type="submit" class="btn btn-default btn-cancel" name="cancel" value="{{__('general.cancel')}}" />
+
+				<input type="submit" class="btn btn-default btn-cancel mr10 flt_lft" name="cancel" value="{{__('general.cancel')}}" />
+				<input type="submit" class="btn btn-primary" value="{{__('general.save')}}" />
 			</form>
 	    </div>
     </div>
