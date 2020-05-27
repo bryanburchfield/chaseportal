93a9b19714905f7f4aa1091a62a3b09fc632f201
Thomas Grauer <tgrauer1@me.com>
2020-05-27 14:58:28 -0400

    Began datatable

--- public/js/compliancedash.js
+++ public/js/compliancedash.js
@@ -42,6 +42,17 @@ Chart.pluginService.register({
 
 var Dashboard = {
 
+    actions_dataTable: $('.agent_compliance_table').DataTable({
+        responsive: true,
+        dom: 'Bfrtip',
+        buttons: [],
+        fnDrawCallback: function(oSettings) {
+            if (oSettings._iDisplayLength >= oSettings.fnRecordsDisplay()) {
+              $(oSettings.nTableWrapper).find('.dataTables_paginate').hide();
+            }
+        }
+    }),
+
     chartColors: {
         red: 'rgb(255,67,77)',
         orange: 'rgb(228,154,49)',
@@ -111,7 +122,36 @@ var Dashboard = {
                         trs += '<tr><td>' + response.agent_compliance.agent_compliance[i].Rep + '</td><td>' + response.agent_compliance.agent_compliance[i].AllowedPausedTime + '</td><td>' + response.agent_compliance.agent_compliance[i].PausedTime + '</td><td>' + response.agent_compliance.agent_compliance[i].PctWorked + '</td><td>' + response.agent_compliance.agent_compliance[i].TotWorkedTime + '</td><td>' + response.agent_compliance.agent_compliance[i].WorkedTime + '</td></tr>';
                     }
 
+                    $('table.agent_compliance_table').DataTable().clear();
+                    $('table.agent_compliance_table').DataTable().destroy();
                     $('.agent_compliance_table tbody').append(trs);
+                    $('table.agent_compliance_table').DataTable({
+                        "bDestroy": true,
+                        "responsive": true,
+                        "language": {
+                            "sEmptyTable":     Lang.get('js_msgs.no_data'),
+                            "sInfo":           Lang.get('js_msgs.info'),
+                            "sInfoEmpty":      Lang.get('js_msgs.info_empty'),
+                            "sInfoFiltered":   Lang.get('js_msgs.info_filtered'),
+                            "sInfoPostFix":    "",
+                            "sInfoThousands":  ",",
+                            "sLengthMenu":     Lang.get('js_msgs.length_menu'),
+                            "sLoadingRecords": Lang.get('js_msgs.loading'),
+                            "sProcessing":     Lang.get('js_msgs.processing'),
+                            "sSearch":         Lang.get('js_msgs.search'),
+                            "sZeroRecords":    Lang.get('js_msgs.zero_records'),
+                            "oPaginate": {
+                                "sFirst":    Lang.get('js_msgs.first'),
+                                "sLast":     Lang.get('js_msgs.last'),
+                                "sNext":     Lang.get('js_msgs.next'),
+                                "sPrevious": Lang.get('js_msgs.previous')
+                            },
+                            "oAria": {
+                                "sSortAscending":  Lang.get('js_msgs.ascending'),
+                                "sSortDescending": Lang.get('js_msgs.descending')
+                            }
+                        }
+                    });
                 }
             }
         });
