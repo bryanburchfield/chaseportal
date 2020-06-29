6dd10138f491f02ca123b6ea020c69e1ff14ab20
Bryan Burchfield <bburchfield@gmail.com>
2020-06-29 12:16:48 -0400

    added TZ dropdown

--- resources/views/dashboards/reportnav.blade.php
+++ resources/views/dashboards/reportnav.blade.php
@@ -7,6 +7,15 @@
 @elseif(session()->has('isSsoSuperadmin'))
     <div class="btn-group flt_rgt">
         <div class="form-group">
+            <select name="tz" id="tz" class="form-control">
+                @foreach ($timezone_array as $key => $name)
+                    <option {{ Auth::user()->tz == $key ? 'selected' : '' }} value="{{$key}}">{{$name}}</option>
+                @endforeach
+            </select>
+        </div>
+    </div>
+    <div class="btn-group flt_rgt">
+        <div class="form-group">
             <select name="group_id" id="group_id" class="form-control">
                 @foreach ($groups as $group)
                     <option {{ Auth::user()->group_id == $group->GroupId ? 'selected' : '' }} value="{{$group->GroupId}}">{{$group->GroupId}} : {{$group->GroupName}}</option>
