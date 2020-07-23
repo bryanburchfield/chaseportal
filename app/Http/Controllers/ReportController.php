31f583effb91d2114546d1a94794d07316c85e7d
Bryan Burchfield <bburchfield@gmail.com>
2020-07-23 11:22:14 -0400

    added results to return from update_report

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -212,6 +212,7 @@ class ReportController extends Controller
         $data = array_merge(['results' => $results], $this->reportservice->getPageData());
 
         return [
+            'results' => $results,
             'table' => view('shared.reporttable')->with($data)->render(),
             'pag' => view('shared.reportpagination')->with($data)->render(),
             'errors' => $errors,
