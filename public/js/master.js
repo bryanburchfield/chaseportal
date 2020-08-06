53b5cd923759c21ddabad01c9d095912cdbae959
Thomas Grauer <tgrauer1@me.com>
2020-08-06 13:15:00 -0400

    update ajax to full path

--- public/js/master.js
+++ public/js/master.js
@@ -962,7 +962,7 @@ var Master = {
             });
 
             $.ajax({
-                url: 'get_campaigns',
+                url: '/dashboards/reports/get_campaigns',
                 type: 'POST',
                 dataType: 'json',
                 async: false, /////////////////////// use async when rebuilding multi select menus
