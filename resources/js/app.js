4074872f100a10fafa3574d7304acdd42212a482
Thomas Grauer <tgrauer1@me.com>
2020-08-12 16:01:02 -0400

    Began storing timeinstatus timers

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,11 +5,15 @@ require('./bootstrap');
 
 function startTime() {
 	var today = new Date();
+	// today = 'Wed Aug 12 2020 13:44:58 GMT-0400 (Eastern Daylight Time)';
 	var h = today.getHours();
 	var m = today.getMinutes();
 	var s = today.getSeconds();
 	m = checkTime(m);
 	s = checkTime(s);
+
+	// console.log(h);
+
 	document.getElementById('txt').innerHTML =
 	h + ":" + m + ":" + s;
 	var t = setTimeout(startTime, 500);
--- resources/views/test/rt_agent_dash.blade.php
+++ resources/views/test/rt_agent_dash.blade.php
@@ -71,55 +71,94 @@
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            <div class="content">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
                     <div class="col-sm-offset-5 col-sm-2 mb20"><div id="txt" class="timer"></div></div>
-                <div class="responsive-table">
-                        <table border="1" class="table realtime_table table-striped table-hover table-bordered">
-                            <thead>
-                                <tr class="success">
-                                    <th>Login</th>
-                                    <th>Campaign</th>
-                                    <th>Subcampaign</th>
-                                    <th>Skill</th>
-                                    <th>TimeInStatus</th>
-                                    <th>BreakCode</th>
-                                    <th>State</th>
-                                    <th>Status</th>
-                                    </th>
-                                </tr>
-                            </thead>
-
-                            <tbody>
-                                @foreach ($data['results'] as $row)
-                                    <tr>
-                                        <td>{{ $row['Login']}}</td>
-                                        <td>{{ $row['Campaign']}}</td>
-                                        <td>{{ $row['Subcampaign']}}</td>
-                                        <td>{{ $row['Skill']}}</td>
-                                        <td>{{ $row['TimeInStatus']}}</td>
-                                        <td>{{ $row['BreakCode']}}</td>
-                                        <td>{{ $row['State']}}</td>
-                                        <td>{{ $row['Status']}}</td>
+                    <div class="responsive-table">
+                            <table border="1" class="table realtime_table table-striped table-hover table-bordered">
+                                <thead>
+                                    <tr class="success">
+                                        <th>Login</th>
+                                        <th>Campaign</th>
+                                        <th>Subcampaign</th>
+                                        <th>Skill</th>
+                                        <th>TimeInStatus</th>
+                                        <th>BreakCode</th>
+                                        <th>State</th>
+                                        <th>Status</th>
+                                        </th>
                                     </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
+                                </thead>
+
+                                <tbody>
+                                    @foreach ($data['results'] as $row)
+                                        <tr>
+                                            <td>{{ $row['Login']}}</td>
+                                            <td>{{ $row['Campaign']}}</td>
+                                            <td>{{ $row['Subcampaign']}}</td>
+                                            <td>{{ $row['Skill']}}</td>
+                                            <td>{{ $row['TimeInStatus']}}</td>
+                                            <td>{{ $row['BreakCode']}}</td>
+                                            <td>{{ $row['State']}}</td>
+                                            <td>{{ $row['Status']}}</td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                    </div>
                 </div>
             </div>
         </div>
         <script src="{{asset('js/app.js')}}"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js" type="text/javascript"></script>
         <script>
+            var ran=false;
             Echo.channel('{{ $channel }}')
                 .listen('NewMessage', (e) => {
-                    console.log(e.message);
+                    console.log(e.message); // 506
+
                     $('.realtime_table tbody').empty();
-                    var trs;
-                    for(var i=0;i<e.message.results.length;i++){
-                        trs+='<tr><td>'+e.message.results[i].Login+'</td><td>'+e.message.results[i].Campaign+'</td><td>'+e.message.results[i].Subcampaign+'</td><td>'+e.message.results[i].Skill+'</td><td>'+e.message.results[i].TimeInStatus+'</td><td>'+e.message.results[i].BreakCode+'</td><td>'+e.message.results[i].State+'</td><td>'+e.message.results[i].Status+'</td>';
+                    var time = '12:05:14';
+
+                    function convert_time(time){
+                        var hours = time.substr(0,2);
+                        var minutes = time.substr(3,2);
+                        var seconds = time.substr(6,2);
+                        //time = hours +':'+ minutes +':'+ seconds;
+                        return hours +':'+ minutes +':'+ seconds;
                     }
 
+                    function start_timer(start_time){
+                        // console.log(start_time);
+                    }
+
+                    function update_timer(new_time){
+                        // console.log(new_time)''
+                    }
+
+                    var trs;
+                    var time_in=[];
+
+                    for(var i=0;i<e.message.results.length;i++){
+
+                        if(!ran){
+                            /// store original time_in_statues
+                            time_in.push(e.message.results[i].TimeInStatus);
+
+                            start_timer(e.message.results[i].TimeInStatus);
+                            console.log(time_in[i]);
+                        }
+
+                        // compare to new time_in_statues result to first one
+                        if(time_in[i] !== e.message.results[i].TimeInStatus){
+                            time_in[i] = e.message.results[i].TimeInStatus;
+                        }
+
+                        trs+='<tr><td>'+e.message.results[i].Login+'</td><td>'+e.message.results[i].Campaign+'</td><td>'+e.message.results[i].Subcampaign+'</td><td>'+e.message.results[i].Skill+'</td><td>'+time_in[i]+'</td><td>'+e.message.results[i].BreakCode+'</td><td>'+e.message.results[i].State+'</td><td>'+e.message.results[i].Status+'</td></tr>';
+                    }
+
+                    ran = true;
                     $('.realtime_table tbody').append(trs);
                 })
         </script>
