5945230a15fc75f2f0bb417bc2fd6c14494b5968
Bryan Burchfield <bburchfield@gmail.com>
2020-08-18 09:16:36 -0400

    js working

--- public/js/realtimeagentdash.js
+++ public/js/realtimeagentdash.js
@@ -7,14 +7,13 @@ var RealTime = {
 			// load each returned array
 			for (var i = 0; i < result.length; i++) {
 				let status_type = result[i][0];
-				status_arrays[status_type] = [];
-				this.load_array(status_type, status_arrays, result[i][1]);
+				status_arrays[status_type] = this.load_array(status_type, result[i][1]);
 			}
 		} else {
 			// process each returned list
 			for (var i = 0; i < result.length; i++) {
 				let status_type = result[i][0];
-				this.process_array(status_type, status_arrays, result[i][1]);
+				status_arrays[status_type] = this.process_array(status_type, status_arrays[status_type], result[i][1]);
 			}
 		}
 
@@ -22,13 +21,17 @@ var RealTime = {
 		ran = true;
 	},
 
-	load_array(list_id, array_data, result_data) {
+	load_array(list_id, result_data) {
+		var return_array = [];
+
 		this.delete_all_rows(list_id);
 
 		for (var i = 0; i < result_data.length; i++) {
-			array_data[list_id].push({ 'Login': result_data[i]['Login'], 'checksum': result_data[i]['checksum'] });
+			return_array.push({ 'Login': result_data[i]['Login'], 'checksum': result_data[i]['checksum'] });
 			this.add_row(list_id, result_data[i]);
 		}
+
+		return return_array;
 	},
 
 	delete_all_rows(list_id) {
@@ -54,40 +57,42 @@ var RealTime = {
 	process_array(list_id, array_data, result_data) {
 		// first, check that there's anything in the returned list
 		if (result_data.length == 0) {
-			if (array_data[list_id].length > 0) {
+			if (array_data.length > 0) {
 				this.delete_all_rows(list_id);
-				array_data[list_id] = [];
+				array_data = [];
 			}
 		} else {
 			// loop thru in-memory list and update or delete as necc
 			// have to do deletes after the loop or things get crazy
 			let delete_list = [];
-			for (var i = 0; i < array_data[list_id].length; i++) {
+			for (var i = 0; i < array_data.length; i++) {
 				// look for login in results
-				let row_obj = result_data.find(row => row.Login === array_data[list_id][i].Login);
+				let row_obj = result_data.find(row => row.Login === array_data[i].Login);
 				if (row_obj) {
-					if (row_obj.checksum != array_data[list_id][i].checksum) {
-						array_data[list_id][i].checksum = row_obj.checksum;
+					if (row_obj.checksum != array_data[i].checksum) {
+						array_data[i].checksum = row_obj.checksum;
 						this.update_row(list_id, row_obj);
 					}
 				} else {
-					delete_list.push(array_data[list_id][i].Login);
+					delete_list.push(array_data[i].Login);
 				}
 			}
 
 			// now delete the ones that need it
 			for (var i = 0; i < delete_list.length; i++) {
-				array_data[list_id] = array_data[list_id].filter(row => row.Login != delete_list[i]);
+				array_data = array_data.filter(row => row.Login != delete_list[i]);
 				this.delete_row(list_id, delete_list[i]);
 			}
 
 			// now go thru the returned list and insert if necc
 			for (var i = 0; i < result_data.length; i++) {
-				if (!array_data[list_id].find(row => row.Login === result_data[i]['Login'])) {
-					array_data[list_id].push({ 'Login': result_data[i]['Login'], 'checksum': result_data[i]['checksum'] });
+				if (!array_data.find(row => row.Login === result_data[i]['Login'])) {
+					array_data.push({ 'Login': result_data[i]['Login'], 'checksum': result_data[i]['checksum'] });
 					this.add_row(list_id, result_data[i]);
 				}
 			}
 		}
+
+		return array_data;
 	}
 }
