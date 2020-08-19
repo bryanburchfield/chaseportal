b7a94ce5dbd18f7b16e89664b441bd2730233484
Thomas Grauer <tgrauer1@me.com>
2020-08-19 14:03:05 -0400

    change to five columns

--- resources/views/shared/realtimeagentdash.blade.php
+++ resources/views/shared/realtimeagentdash.blade.php
@@ -5,7 +5,12 @@
 <div class="row">
 
     @foreach ($statuses as $status)
-        <div class="col-sm-2 rep_status {{$status}}">
+        @if(!$loop->index)
+            <div class="col-sm-7 five-three p0">
+        @elseif($loop->index == 3)
+            <div class="col-sm-5 five-two p0">
+        @endif
+        <div class="col-sm-{{$loop->index < 3 ? '4': '6'}} rep_status {{$status}}">
             <h2 class="mb0">{{__('general.' . $status)}}</h2>
             <div class="num_agents">
                 <div class="inner">{{count($data['data'][$status])}}</div>
@@ -22,6 +27,10 @@
                 @endforeach
             </ul>
         </div>
+
+        @if($loop->index % 2 == 0 && $loop->index !=0)
+            </div>
+        @endif
     @endforeach
 
 </div>
