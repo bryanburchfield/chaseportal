c536e1621bbfdb82d5133b27e0f5f8d9465b66c7
Thomas Grauer <tgrauer1@me.com>
2020-08-19 12:43:20 -0400

    Style agent columns

--- resources/views/shared/realtimeagentdash.blade.php
+++ resources/views/shared/realtimeagentdash.blade.php
@@ -6,13 +6,13 @@
 
     @foreach ($statuses as $status)
         <div class="col-sm-2 rep_status {{$status}}">
-            <h2>{{__('general.' . $status)}}</h2>
+            <h2 class="mb0">{{__('general.' . $status)}}</h2>
             <div class="num_agents">
                 <div class="inner">{{count($data['data'][$status])}}</div>
                 <p>{{__('general.agents')}}</p>
             </div>
 
-            <ul id="{{$status}}" class="{{$status}} list-group">
+            <ul id="{{$status}}" class="{{$status}} list-group cb">
                 @foreach($data['data'][$status] as $record)
                     <li class="list-group-item">
                         <p data-checksum="{{$record['checksum']}}" class="rep_name mb0">{{$record['Login']}}</p>
