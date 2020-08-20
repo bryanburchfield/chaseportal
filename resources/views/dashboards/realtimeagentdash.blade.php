477c1890575e129f118e14fbf8c259cac5cf2fd0
Bryan Burchfield <bburchfield@gmail.com>
2020-08-20 11:59:15 -0400

    removed camp/date header

--- resources/views/dashboards/realtimeagentdash.blade.php
+++ resources/views/dashboards/realtimeagentdash.blade.php
@@ -1,15 +1,4 @@
 
 <div class="container-full mt20">
-    <div class="row">
-        <div class="col-sm-12">
-            <div class="filter_time_camp_dets">
-                <p>
-                    <span class="selected_datetime"></span> |
-                    <span class="selected_campaign"></span>
-                </p>
-            </div>
-        </div>
-    </div>
-
     @include('shared.realtimeagentdash')
 </div>
