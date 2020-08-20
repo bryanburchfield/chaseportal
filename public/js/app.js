3eeb2495cdc37c624492bcd22fd7a445aeff3e88
Thomas Grauer <tgrauer1@me.com>
2020-08-20 12:53:52 -0400

    remove clock

--- public/js/app.js
+++ public/js/app.js
@@ -25160,32 +25160,7 @@ module.exports = function(module) {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js"); /// clock
-
-
-function startTime() {
-  var today = new Date(); // today = 'Wed Aug 12 2020 13:44:58 GMT-0400 (Eastern Daylight Time)';
-
-  var h = today.getHours();
-  var m = today.getMinutes();
-  var s = today.getSeconds();
-  m = checkTime(m);
-  s = checkTime(s); // console.log(h);
-
-  document.getElementById('txt').innerHTML = h + ":" + m + ":" + s;
-  var t = setTimeout(startTime, 500);
-}
-
-function checkTime(i) {
-  if (i < 10) {
-    i = "0" + i;
-  }
-
-  ;
-  return i;
-}
-
-startTime();
+__webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
 
 /***/ }),
 
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,25 +1,3 @@
 require('./bootstrap');
 
 
-/// clock
-
-function startTime() {
-	var today = new Date();
-	// today = 'Wed Aug 12 2020 13:44:58 GMT-0400 (Eastern Daylight Time)';
-	var h = today.getHours();
-	var m = today.getMinutes();
-	var s = today.getSeconds();
-	m = checkTime(m);
-	s = checkTime(s);
-
-	// console.log(h);
-
-	document.getElementById('txt').innerHTML = h + ":" + m + ":" + s;
-	var t = setTimeout(startTime, 500);
-}
-function checkTime(i) {
-	if (i < 10) {i = "0" + i};
-	return i;
-}
-
-startTime();
