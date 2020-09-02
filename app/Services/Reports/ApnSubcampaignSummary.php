68e84b5035515521eebadc8461f4deb8b783a008
Bryan Burchfield <bburchfield@gmail.com>
2020-09-02 14:03:26 -0400

    working on apn_subcamp report

--- app/Services/Reports/ApnSubcampaignSummary.php
+++ app/Services/Reports/ApnSubcampaignSummary.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use \App\Traits\ReportTraits;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Log;
 
 class ApnSubcampaignSummary
 {
@@ -473,6 +474,9 @@ class ApnSubcampaignSummary
             $offset = ($this->params['curpage'] - 1) * $this->params['pagesize'];
             $sql .= " OFFSET $offset ROWS FETCH NEXT " . $this->params['pagesize'] . " ROWS ONLY";
         }
+
+        Log::debug($sql);
+        Log::debug($bind);
 
         return [$sql, $bind];
     }
