a0127c1c777967f6d906a365d345cf297e2fa234
Thomas Grauer <tgrauer1@me.com>
2020-09-22 14:49:21 -0400

    select all toggle

--- public/js/reports.js
+++ public/js/reports.js
@@ -3,6 +3,13 @@ if($('#ecoverme_lead_export').length){
 	$('#call_status_select ').multiselect('select', 'Client Sold');
 }
 
+$('#group_select').next('ul').on('click', '.select_all_groups', function(){
+	if($(this).is(':checked')){
+		$(this).parent().parent().siblings().find('input').prop("checked", true);
+	}else{
+		$(this).parent().parent().siblings().find('input').prop("checked", false);
+	}
+});
 
 $('#group_duration #dialer').on('change', function(){
 	var dialer = $(this).val();
@@ -27,7 +34,7 @@ $('#group_duration #dialer').on('change', function(){
         	// build groups select menu
         	if(response.length){
 
-        		var groups_list='';
+        		var groups_list='<div class="select_all checkbox mb10 cb"><label><input class="select_all_groups" name="select_all_groups" type="checkbox" value=""><b>Select All</b></label></div>';
         		for(var i=0; i<response.length;i++){
         		    groups_list+='<div class="checkbox mb10 cb"><label><input class="groups" name="groups[]" type="checkbox" value="'+response[i]['GroupId']+'"><b>'+response[i]['GroupName']+'</b></label></div>';
         		}
