951a3399126289418f2bb7dc9eaf3d050643c4a9
Thomas Grauer <tgrauer1@me.com>
2020-10-30 09:57:29 -0400

    form styles, code preview, one click copy

--- public/js/formbuilder.js
+++ public/js/formbuilder.js
@@ -1,40 +1,67 @@
 var FORMBUILDER = {
+
+	element_type:'',
+
 	init:function(){
 		$('.add_element').on('click', this.add_element);
 		$('.form_preview').on('click', '.remove_form_element', this.remove_element);
 		$('.theme_selector select').on('change', this.apply_theme);
 		$('body').on('click', '.edit_form_element', this.pass_id);
 		$('.edit_field').on('click', this.update_field);
+		$('body').on('click', '.edit_form_element', this.populate_fields_form);
+		$('.form_code').on('click', this.copy_code);
+		$('.display_type').on('change', this.change_checked_type);
 	},
 
-	// add each element to its own to pre tag to avoid formatting barfs
 	add_element:function(e){
 		e.preventDefault();
-		var element = $(this).parent().next().find('.form-group').html();
-		console.log($(this).parent().next().find('.form-control').attr('type'));
-		var id= $('.user_created_form_element').length;
-		var appended_elem = $('<div class="user_created_form_element" data-id="'+id+'"><div class="col-sm-1"><a class="remove_form_element text-center mr5" href="#"><i class="fas fa-trash-alt"></i></a><a class="edit_form_element text-center" href="#" data-toggle="modal" data-target="#editFieldModal"><i class="fas fa-edit"></i></a></div><div class="col-sm-11 user_created_element"><div class="form-group">'+element+'</div></div></div>');
+		var type = $(this).data('type');
+		FORMBUILDER.element_type = type;
+		FORMBUILDER.show_numb_fields();
 
-		$(appended_elem).appendTo('.form_preview');
-		$('.form_preview').show();
-		FORMBUILDER.update_code();
+		if(type=='radio' || type =='checkbox'){
+			$('#editFieldModal').modal('show');
+		}else{
+			var element = $(this).parent().next().find('.form-group').html();
+
+			var id= $('.user_created_form_element').length;
+			var appended_elem = $('<div class="user_created_form_element" data-id="'+id+'"><div class="col-sm-1"><a class="remove_form_element text-center mr5" href="#"><i class="fas fa-trash-alt"></i></a><a class="edit_form_element text-center" href="#" data-toggle="modal" data-target="#editFieldModal"><i class="fas fa-edit"></i></a></div><div class="col-sm-11 user_created_element"><div class="form-group">'+element+'</div></div></div>');
+
+			$(appended_elem).appendTo('.form_preview');
+
+			// remove submit btn then append to bottom
+			$('.form_preview').find('.btn_div').remove();
+			var btn = $('<div class="user_created_form_element btn_div"><div class="col-sm-1"></div><div class="col-sm-11 user_created_element"><input type="submit" control="submit" action="submit_and_navigate" navigate-to="confirmation_page" value="Submit and Navigate" class="control-submit btn btn-primary"></div></div>');
+			$('.form_preview').find('.btn').remove();
+			$(btn).appendTo('.form_preview');
+
+			$('.form_preview').show();
+			$('.form_code').parent().show();
+			FORMBUILDER.update_code();
+		}
+
+		console.log(FORMBUILDER.element_type);
 	},
 
 	remove_element:function(e){
 		e.preventDefault();
 		$(this).parent().parent().remove();
 		FORMBUILDER.update_code();
+		if(!$('.user_created_form_element').length){
+			$('.form_code').parent().hide();
+			$('.form_preview').hide();
+		}
 	},
 
 	apply_theme:function(){
-		
+
 		if($(this).val()!='clean'){
-			$('.form_preview, .form_element_options').find('input.form-control').each(function(){
+			$('.form_preview, .form_element_options').find('.form-control').each(function(){
 				$(this).parent().find('label').show();
 			});
-			$('.form_preview, .form_element_options').find('input.form-control').attr('placeholder', '');
+			$('.form_preview, .form_element_options').find('.form-control').attr('placeholder', '');
 		}else{
-			$('.form_preview, .form_element_options').find('input.form-control').each(function(){
+			$('.form_preview, .form_element_options').find('.form-control').each(function(){
 				var label=$(this).parent().find('label').text();
 				$(this).attr('placeholder', label);
 				$(this).parent().find('label').hide();
@@ -47,26 +74,29 @@ var FORMBUILDER = {
 		$('.form_element_options').find('.form-control').attr('class', 'form-control');
 		$('.form_element_options').find('.form-control').addClass($(this).val());
 
-		FORMBUILDER.update_code();
+		if($('.user_created_form_element').length){
+			FORMBUILDER.update_code();
+		}
 	},
 
 	update_code:function(){
-		$('.codegoeshere').empty();
-		
+		$('.form_code').empty();
 
 		$('.user_created_form_element').find('.user_created_element').each(function(){
-			var html=$(this).html().trim();
-			// console.log($(this).html());
-			// html = html.replace(/^\s*/gm, '');
+			var html=$(this).html();
 			// console.log(html);
+			// html = html.replace(/^\s*/gm, '');
 			html=html.replace(/</g, "&lt;");
 			html=html.replace(/>/g, "&gt;");
-			$('.codegoeshere').append(html);
-			
+
+			// $('.copy_code pre code').append(html);
+
+			var new_code_block = $('<pre class="p10 appended_code sh_html xml"></pre>').wrapInner(html);
+			$('.form_code').append(new_code_block);
 		});
 
 		$('.form_code').parent().show();
-		
+
 		$('pre').each(function(i, block) {
 		    hljs.highlightBlock(block);
 		});
@@ -77,16 +107,69 @@ var FORMBUILDER = {
 	},
 
 	update_field:function(){
+
 		var field_label = $('.field_label').val();
 		var field_name = $('.field_name').val();
 		var id = $('.id').val();
-		
+
 		$('.form_preview').find('[data-id='+id+']').find('.form-group label').text(field_label);
+		$('.form_preview').find('[data-id='+id+']').find('.form-group .form-control').attr('placeholder', field_label);
 		$('.form_preview').find('[data-id='+id+']').find('.form-group .form-control').attr('name', field_name);
+		$('.form_preview').find('[data-id='+id+']').find('.form-group .form-control').attr('field-name', field_name);
+		$('.form_preview').find('[data-id='+id+']').addClass('edited');
 
 		$('#editFieldModal').modal('hide');
 		FORMBUILDER.update_code();
-	}
+	},
+
+	populate_fields_form:function(){
+
+		var id = $(this).parent().parent().data('id');
+
+		if($(this).parent().parent().hasClass('edited')){
+			$('.field_label').val($(this).parent().parent().find('label').text());
+			$('.field_name').val($(this).parent().parent().find('.form-control').attr('name'));
+		}
+	},
+
+	show_numb_fields:function(){
+		$('#editFieldModal').find('.modal-body .hidetilloaded').show();
+	},
+
+	change_checked_type:function(){
+		if($(this).val()=='stacked'){
+			$('.inline').hide();
+			$('.stacked').show();
+			// find radio with value of stacked and set that to checked
+			// $('.stacked').find('input').val('')
+		}else{
+			$('.inline').show();
+			$('.stacked').hide();
+			// find radio with value of inline and set that to checked
+			// $('.inline').find('input').val('')
+		}
+	},
+
+	copy_code: function (e) {
+        e.preventDefault();
+        console.log('clicked');
+        $(this).tooltip({
+            animated: 'fade',
+            placement: 'left',
+            trigger: 'click'
+        });
+
+        setTimeout(function () {
+            $('.tooltip').fadeOut('slow');
+        }, 3500);
+
+        var $temp = $("<input>");
+        $(this).parent().append($temp);
+        $temp.val($(this).text()).select();
+        document.execCommand("copy");
+        console.log($temp);
+        $temp.remove();
+    },
 }
 
 $(document).ready(function(){
@@ -95,5 +178,6 @@ $(document).ready(function(){
 	//clear form when modal is closed
 	$('#editFieldModal').on("hide.bs.modal", function() {
 		$(this).find('.modal-body .form-control').val('');
+		$(this).find('.modal-body .hidetilloaded').hide();
 	})
 });
--- resources/views/tools/form_builder.blade.php
+++ resources/views/tools/form_builder.blade.php
@@ -23,35 +23,35 @@
 			    					<option value="">Select One</option>
 			    					<option value="light">Light</option>
 			    					<option value="clean">Clean</option>
-			    					<option value="rounded_borders">Rounded Borders</option>
-			    					<option value="another_one">Something Else</option>
+			    					<option value="rounded">Rounded</option>
+			    					<option value="focus">Focus</option>
 			    				</select>
 			    			</div>
-			    			
+
 <div class="form_element_options">
 <div class="form_option">
 <div class="col-sm-2 p0">
-	<a href="#" class="add_element mt10 input"><i class="fas fa-plus-circle"></i> Add</a>
+	<a href="#" data-type="input" class="add_element mt10 input"><i class="fas fa-plus-circle"></i> Add</a>
 </div>
-			    					
+
 <div class="col-sm-10">
 <div class="form-group">
 	<label>Label</label>
-	<input type="text" class="form-control default" placeholder="" name="" value="">
+	<input type="text" class="form-control default" placeholder="" name="" field-name="" value="">
 </div>
 </div>
 </div>
 
 <div class="form_option">
 <div class="col-sm-2 p0">
-	<a href="#" class="add_element mt10 textarea"><i class="fas fa-plus-circle"></i> Add</a>
+	<a href="#" data-type="textarea" class="add_element mt10 textarea"><i class="fas fa-plus-circle"></i> Add</a>
 </div>
 
 <div class="col-sm-10">
 
 <div class="form-group">
 	<label>Label</label>
-	<textarea class="form-control default" rows="3"></textarea>
+	<textarea class="form-control default" rows="3" placeholder=""></textarea>
 </div>
 
 </div>
@@ -59,7 +59,7 @@
 
 <div class="form_option">
 <div class="col-sm-2 p0">
-	<a href="#" class="add_element mt10 checkbox"><i class="fas fa-plus-circle"></i> Add</a>
+	<a href="#" data-type="checkbox" class="add_element mt10 checkbox"><i class="fas fa-plus-circle"></i> Add</a>
 </div>
 
 <div class="col-sm-10">
@@ -73,32 +73,29 @@
 
 <div class="form_option">
 <div class="col-sm-2 p0">
-	<a href="#" class="add_element mt10 radio"><i class="fas fa-plus-circle"></i> Add</a>
+	<a href="#" data-type="radio" class="add_element mt10 radio"><i class="fas fa-plus-circle"></i> Add</a>
 </div>
 
 <div class="col-sm-10">
 <div class="form-group">
 	<label class="radio-inline"><input type="radio" name="optradio" value="option1"> Option one</label>
-	{{-- <label class="radio-inline"><input type="radio" name="optradio" value="option1"> Option one</label>
-	<label class="radio-inline"><input type="radio" name="optradio" value="option1"> Option one</label> --}}
+	<label class="radio-inline"><input type="radio" name="optradio" value="option1"> Option one</label>
+	<label class="radio-inline"><input type="radio" name="optradio" value="option1"> Option one</label>
 </div>
 </div>
 </div>
+
 </div>
-</div>						
+</div>
 </div>
 
 					<div class="col-sm-8">
-						<div class="card form_preview oa hidetilloaded">
-							
-						</div>
+						<div class="card form_preview oa hidetilloaded"></div>
+						<div class="copy_code hidetilloaded"><pre><code></code></pre></div>
 
-						<div class="card hidetilloaded">
-							<div class="form_code">
-								<pre class="p10 sh_html codegoeshere">
-									{{-- <code class="html codegoeshere p10">
-									</code> --}}
-								</pre>
+						<div class="card hidetilloaded form_code_preview">
+							<div class="form_code" data-toggle="tooltip"  title="Link Copied!">
+								{{-- <pre class="p10 appended_code sh_html btn_code xml"><input type="submit" control="submit" action="submit_and_navigate" navigate-to="confirmation_page" value="Submit and Navigate" class="control-submit btn btn-primary"></pre> --}}
 							</div>
 						</div>
 					</div>
@@ -133,11 +130,42 @@
             		<input type="text" class="field_name form-control" name="field_name">
             	</div>
 
+            	<div class="form-group hidetilloaded">
+            		<label># of Fields</label>
+            		<select name="numb_fields" id="numb_fields" class="form-control">
+            			<option value="">Select One</option>
+            			<option value="1">1</option>
+            			<option value="2">2</option>
+            			<option value="3">3</option>
+            			<option value="4">4</option>
+            			<option value="5">5</option>
+            			<option value="6">6</option>
+            			<option value="7">7</option>
+            			<option value="8">8</option>
+            		</select>
+            	</div>
+
+            	<div class="form-group hidetilloaded stacked">
+            		<div class="radio">
+						<label><input type="radio" name="display_type" class="display_type" value="inline" checked>Inline</label>
+	            	</div>
+
+	            	<div class="radio">
+						<label><input type="radio" name="display_type" class="display_type" value="stacked" checked>Stacked</label>
+	            	</div>
+            	</div>
+
+            	<div class="form-group hidetilloaded mt20 inline">
+            		<h5 class="mb5"><b>Display Type</b></h5>
+            		<label class="radio-inline"><input type="radio" class="display_type" name="display_type" value="inline" checked> Inline</label>
+            		<label class="radio-inline"><input type="radio" class="display_type" name="display_type" value="stacked"> Stacked</label>
+            	</div>
+
             	<input type="hidden" class="id" value="">
             </div>
            
             <div class="modal-footer">
-                <button type="button" class="btn btn-default mr10 flt_lft" data-dismiss="modal">{{__('general.close')}}</button>
+                <button type="button" class="btn btn-default mr10 flt_lft" data-dismiss="modal" tabindex="-1">{{__('general.close')}}</button>
                 <a href="#" class="edit_field btn-primary btn flt_lft">{{__('general.save')}}</a>
             </div>
             {!! Form::close() !!}
