1e6d1f38382cfc68a4645fa639ef00e64cc23da5
Bryan Burchfield <bburchfield@gmail.com>
2020-11-05 16:06:32 -0500

    spamcheck with twilio/nomorobo

--- config/twilio.php
+++ config/twilio.php
@@ -5,4 +5,6 @@ return [
     'from' => env('TWILIO_FROM', ''),
     'did_sid' => env('TWILIO_DID_SID', ''),
     'did_token' => env('TWILIO_DID_TOKEN', ''),
+    'spam_sid' => env('TWILIO_SPAM_SID', ''),
+    'spam_token' => env('TWILIO_SPAM_TOKEN', ''),
 ];
