7b777d391416b4ec70f83a21d7b34723c0edfa6c
Thomas Grauer <tgrauer1@me.com>
2020-11-13 15:43:45 -0500

    button types, sortable fields, various tweaks

--- resources/views/tools/form_builder.blade.php
+++ resources/views/tools/form_builder.blade.php
@@ -33,6 +33,12 @@
 									<select name="client_table" id="client_table_menu" class="form-control"></select>
 								</div>
 
+								<div class="form-group">
+									<h4 class="mb10">Submit Button Type</h4>
+									<label class="radio-inline"><input class="submit_btn_type" value="submit" type="radio" name="submit_btn_type" checked>Submit</label>
+									<label class="radio-inline"><input class="submit_btn_type" value="submit_navigate" type="radio" name="submit_btn_type">Submit and Navigate</label>
+								</div>
+
 								<div class="alert alert-danger hidetilloaded"></div>
 							</div>
 						</div>
@@ -54,32 +60,36 @@
 										</div>
 									</div>
 
-									@foreach($default_lead_fields as $val)
-										<div class="field">
-											<div class="col-sm-1">
-												<a href="#" class="remove_field"><i class="fas fa-times-circle"></i></a>
-											</div>
-											<div class="col-sm-4">
-												<p class="field_label" data-field="{{$val}}">{{$val}}</p>
-											</div>
+									<div class="all-slides">
+										@foreach($default_lead_fields as $val)
+											<div class="field slide">
+												<div class="col-sm-1">
+													<a href="#" class="remove_field"><i class="fas fa-times-circle"></i></a>
+												</div>
+												<div class="col-sm-4">
+													<p class="field_label" data-field="{{$val}}">{{$val}}</p>
+												</div>
 
-											<div class="col-sm-5">
-												<div class="form-group">
-													<input type="text" class="form-control field_name" name="{{$val}}" placeholder="{{$val}}" value="{{$val}}">
+												<div class="col-sm-5">
+													<div class="form-group">
+														<input type="text" class="form-control field_name" name="{{$val}}" placeholder="{{$val}}" value="{{$val}}">
+													</div>
+												</div>
+
+												<div class="col-sm-2">
+													{{-- <div class="form-group">
+														<select name="field_type" class="form-control field_type">
+															<option value="">Select One</option>
+															<option value="input">Input</option>
+															<option value="textarea">Textarea</option>
+														</select>
+													</div> --}}
 												</div>
 											</div>
+										@endforeach
+									</div>
 
-											<div class="col-sm-2">
-												{{-- <div class="form-group">
-													<select name="field_type" class="form-control field_type">
-														<option value="">Select One</option>
-														<option value="input">Input</option>
-														<option value="textarea">Textarea</option>
-													</select>
-												</div> --}}
-											</div>
-										</div>
-									@endforeach
+									<div class="cloned-slides" id="cloned-slides"></div>
 								</div>
 
 								<div class="col-sm-5 p0">
@@ -97,69 +107,69 @@
 
 <div class="hidetilloaded html_options">
 <div class="input">
-<div class="form-group">
-	<label>Label</label>
-	<input type="text" class="form-control" name="label" field-name="label" id="label">
-</div>
+	<div class="form-group">
+		<label>Label</label>
+		<input type="text" class="form-control" name="label" field-name="label" id="label">
+	</div>
 </div>
 
 <div class="select_state">
-<div class="form-group">
-	<select name="state" id="state">
-		<option value="" selected="selected">Select a State</option>
-		<option value="AL">Alabama</option>
-		<option value="AK">Alaska</option>
-		<option value="AZ">Arizona</option>
-		<option value="AR">Arkansas</option>
-		<option value="CA">California</option>
-		<option value="CO">Colorado</option>
-		<option value="CT">Connecticut</option>
-		<option value="DE">Delaware</option>
-		<option value="DC">District Of Columbia</option>
-		<option value="FL">Florida</option>
-		<option value="GA">Georgia</option>
-		<option value="HI">Hawaii</option>
-		<option value="ID">Idaho</option>
-		<option value="IL">Illinois</option>
-		<option value="IN">Indiana</option>
-		<option value="IA">Iowa</option>
-		<option value="KS">Kansas</option>
-		<option value="KY">Kentucky</option>
-		<option value="LA">Louisiana</option>
-		<option value="ME">Maine</option>
-		<option value="MD">Maryland</option>
-		<option value="MA">Massachusetts</option>
-		<option value="MI">Michigan</option>
-		<option value="MN">Minnesota</option>
-		<option value="MS">Mississippi</option>
-		<option value="MO">Missouri</option>
-		<option value="MT">Montana</option>
-		<option value="NE">Nebraska</option>
-		<option value="NV">Nevada</option>
-		<option value="NH">New Hampshire</option>
-		<option value="NJ">New Jersey</option>
-		<option value="NM">New Mexico</option>
-		<option value="NY">New York</option>
-		<option value="NC">North Carolina</option>
-		<option value="ND">North Dakota</option>
-		<option value="OH">Ohio</option>
-		<option value="OK">Oklahoma</option>
-		<option value="OR">Oregon</option>
-		<option value="PA">Pennsylvania</option>
-		<option value="RI">Rhode Island</option>
-		<option value="SC">South Carolina</option>
-		<option value="SD">South Dakota</option>
-		<option value="TN">Tennessee</option>
-		<option value="TX">Texas</option>
-		<option value="UT">Utah</option>
-		<option value="VT">Vermont</option>
-		<option value="VA">Virginia</option>
-		<option value="WA">Washington</option>
-		<option value="WV">West Virginia</option>
-		<option value="WI">Wisconsin</option>
-		<option value="WY">Wyoming</option>
-	</select>
-</div>
+	<div class="form-group">
+		<select name="state" id="state">
+			<option value="" selected="selected">Select a State</option>
+			<option value="AL">Alabama</option>
+			<option value="AK">Alaska</option>
+			<option value="AZ">Arizona</option>
+			<option value="AR">Arkansas</option>
+			<option value="CA">California</option>
+			<option value="CO">Colorado</option>
+			<option value="CT">Connecticut</option>
+			<option value="DE">Delaware</option>
+			<option value="DC">District Of Columbia</option>
+			<option value="FL">Florida</option>
+			<option value="GA">Georgia</option>
+			<option value="HI">Hawaii</option>
+			<option value="ID">Idaho</option>
+			<option value="IL">Illinois</option>
+			<option value="IN">Indiana</option>
+			<option value="IA">Iowa</option>
+			<option value="KS">Kansas</option>
+			<option value="KY">Kentucky</option>
+			<option value="LA">Louisiana</option>
+			<option value="ME">Maine</option>
+			<option value="MD">Maryland</option>
+			<option value="MA">Massachusetts</option>
+			<option value="MI">Michigan</option>
+			<option value="MN">Minnesota</option>
+			<option value="MS">Mississippi</option>
+			<option value="MO">Missouri</option>
+			<option value="MT">Montana</option>
+			<option value="NE">Nebraska</option>
+			<option value="NV">Nevada</option>
+			<option value="NH">New Hampshire</option>
+			<option value="NJ">New Jersey</option>
+			<option value="NM">New Mexico</option>
+			<option value="NY">New York</option>
+			<option value="NC">North Carolina</option>
+			<option value="ND">North Dakota</option>
+			<option value="OH">Ohio</option>
+			<option value="OK">Oklahoma</option>
+			<option value="OR">Oregon</option>
+			<option value="PA">Pennsylvania</option>
+			<option value="RI">Rhode Island</option>
+			<option value="SC">South Carolina</option>
+			<option value="SD">South Dakota</option>
+			<option value="TN">Tennessee</option>
+			<option value="TX">Texas</option>
+			<option value="UT">Utah</option>
+			<option value="VT">Vermont</option>
+			<option value="VA">Virginia</option>
+			<option value="WA">Washington</option>
+			<option value="WV">West Virginia</option>
+			<option value="WI">Wisconsin</option>
+			<option value="WY">Wyoming</option>
+		</select>
+	</div>
 </div>
 </div>
 
