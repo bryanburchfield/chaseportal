63160c4c5bba6ef634cae15d038872dc18508b5b
Bryan Burchfield <bburchfield@gmail.com>
2020-12-03 11:12:57 -0500

    test psql

--- config/database.php
+++ config/database.php
@@ -60,22 +60,17 @@ return [
         ],
 
         'phoneflags' => [
-            'driver' => 'mysql',
+            'driver' => 'pgsql',
             'host' => env('PFDB_HOST', '127.0.0.1'),
-            'port' => env('PFDB_PORT', '3306'),
+            'port' => env('PFDB_PORT', '5432'),
             'database' => env('PFDB_DATABASE'),
             'username' => env('PFDB_USERNAME'),
             'password' => env('PFDB_PASSWORD'),
-            'unix_socket' => env('PFDB_SOCKET'),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
+            'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            'schema' => 'public',
+            'sslmode' => 'prefer',
         ],
 
         'pgsql' => [
