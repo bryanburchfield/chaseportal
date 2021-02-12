c7765855997cb389cb3681d5a4aedd688670c31a
Bryan Burchfield <bburchfield@gmail.com>
2021-02-12 14:40:37 -0500

    added ans mach codes back as connects

--- app/Traits/SqlServerTraits.php
+++ app/Traits/SqlServerTraits.php
@@ -121,25 +121,6 @@ trait SqlServerTraits
     {
         // goofy hard-coded list of call statuses used for calculating connects
         return [
-            'AM - Answering Machine',
-            'ANS MACH',
-            'ANS MACHINE',
-            'ANS',
-            'ANS. MAC. DAY',
-            'Ans Mach',
-            'Ans Machine',
-            'Ans. Mach + VM left',
-            'Ans. Mach. / Not Avail.',
-            'Ans. Mach/Not Avail',
-            'Ans. Machine / Left Message',
-            'Ans. Machine',
-            'Ans. Machine, VM not avail',
-            'Ans. Machine/Voicemail',
-            'Answer Machine',
-            'Answer. Machine',
-            'Answering Machine Detected',
-            'Answering Machine',
-            'Answering Service',
             'CR_BAD_NUMBER',
             'CR_BUSY',
             'CR_CEPT',
@@ -163,12 +144,6 @@ trait SqlServerTraits
             'TRANSFERRED',
             'UNFINISHED',
             'UNKNOWN',
-            'VM Full',
-            'VOICE MAIL',
-            'VoiceMail Message Delivered',
-            'Voicemail Box is full',
-            'Voicemail Full',
-            'voicemail',
         ];
     }
 
