669526c050002471e4b3c2a22e852093fcf4b8ef
Thomas Grauer <tgrauer1@me.com>
2021-02-19 09:09:01 -0500

    Updated cdn links for bs/jq in layout blades

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -12,7 +12,8 @@
     <meta name="es.tools.tools" >
     <meta name="_token" content="{{csrf_token()}}" />
     <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png">
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+   
     @isset($cssfile)
         @foreach($cssfile as $css)
             <link href="{{ $css }}" rel="stylesheet">
@@ -55,8 +56,9 @@
     @yield('content')
 
     <script src="/messages.js"></script> 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js" type="text/javascript"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.bundle.min.js" type="text/javascript"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.12/js/jquery.dataTables.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.12/js/dataTables.bootstrap.min.js"></script>
@@ -103,7 +105,7 @@
     @isset($page)
         @if($page['type'] !='dash')
                 <script>
-                $(window).load(function() {
+                $(window).on('load', function() {
                     $('.preloader').fadeOut('slow');
                 });
             </script>
--- resources/views/shared/navbar.blade.php
+++ resources/views/shared/navbar.blade.php
@@ -1,10 +1,11 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-
-       <div class="col-xs-1 col-sm-5 brand pl0">
-           <button type="button" id="sidebarCollapse" class="btn flt_lft">
-               <i class="fas fa-align-left"></i>
-           </button>
+       <div class="col-xs-2 col-sm-6 brand pl0">
+           @if(!session()->has('isSso'))
+            <button type="button" id="sidebarCollapse" class="btn flt_lft">
+                <i class="fas fa-align-left"></i>
+            </button>
+           @endif
 
            @if(Auth::user()->theme == 'dark')
                <img src="/img/text_logo_light.png" alt="" class="img-responsive text_logo flt_lft">
@@ -13,56 +14,52 @@
            @endif
        </div>
 
-        <div class="filters col-xs-11 col-sm-7">
-            <div class="input-group">
-                <div class="input-group-btn">
+        <div class="filters col-xs-10 col-sm-6">
+            
+            @if(!session()->has('isSso'))
+                {!! Form::open(['method'=>'GET', 'action'=>'Auth\LoginController@logout', 'id'=> 'logout-form']) !!}
+                    @csrf
+                    <div class="btn-group float-right d-flex"">
+                    {!! Form::submit(__('general.logout'),['class'=>'btn logout_btn']) !!}
+                    </div>
+                {!! Form::close() !!}
+            @endif
 
-                    @if(!session()->has('isSso'))
-                        {!! Form::open(['method'=>'GET', 'action'=>'Auth\LoginController@logout', 'id'=> 'logout-form']) !!}
-                            @csrf
-                            <div class="btn-group flt_rgt">
-                            {!! Form::submit(__('general.logout'),['class'=>'btn logout_btn']) !!}
-                            </div>
-                        {!! Form::close() !!}
-                    @endif
+            @if($page['type'] =='dash')
+                @include('dashboards.dashnav')
+            @elseif($page['type'] == 'kpi_page')
+                @include('dashboards.kpinav')
+            @elseif($page['type'] == 'report')
+                @include('dashboards.reportnav')
+            @elseif($page['type'] == 'recipients')
+                @include('dashboards.recipientsnav')
+            @endif
 
-                    @if($page['type'] =='dash')
-                        @include('dashboards.dashnav')
-                    @elseif($page['type'] == 'kpi_page')
-                        @include('dashboards.kpinav')
-                    @elseif($page['type'] == 'report')
-                        @include('dashboards.reportnav')
-                    @elseif($page['type'] == 'recipients')
-                        @include('dashboards.recipientsnav')
-                    @endif
+            @if(Auth::user()->language_displayed)
+                <div class="dropdown float-right">
+                    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+                        <span><i class="fas fa-globe-americas"></i> Language</span>
+                    </button>
 
-                    @if(Auth::user()->language_displayed)
-                        <li class="btn-group flt_rgt">
-                            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
-                                <span><i class="fas fa-globe-americas"></i> Language</span>
-                            </button>
-
-                            <ul class="dropdown-menu lang_select stop-propagation">
-                                <li><a class="dropdown-item" href="{{url('lang/en')}}">English</a></li>
-                                <li><a class="dropdown-item" href="{{url('lang/es')}}"> Español</a></li>
-                            </ul>
-                        </li>
-                    @endif
-
-                     @if(!session()->has('isSso'))
-                        <li class="notifications btn-group flt_rgt">
-                            <button type="button" class="btn btn-notifications">
-                                <span>
-                                    <i class="fas fa-bell"></i>
-                                    @if(Auth::user()->unreadFeatureMessagesCount() != 0)
-                                        <span class="numb_notifications">{{Auth::user()->unreadFeatureMessagesCount()}}</span>
-                                    @endif
-                                </span>
-                            </button>
-                        </li>
-                    @endif
+                    <div class="dropdown-menu dropdown-menu-right py-0 stop-propagation">
+                        <a class="dropdown-item" href="{{url('lang/en')}}">English</a>
+                        <a class="dropdown-item" href="{{url('lang/es')}}"> Español</a>
+                    </div>
                 </div>
-            </div>
+            @endif
+
+             @if(!session()->has('isSso'))
+                <li class="notifications btn-group float-right d-flex"">
+                    <button type="button" class="btn btn-notifications">
+                        <span>
+                            <i class="fas fa-bell"></i>
+                            @if(Auth::user()->unreadFeatureMessagesCount() != 0)
+                                <span class="numb_notifications">{{Auth::user()->unreadFeatureMessagesCount()}}</span>
+                            @endif
+                        </span>
+                    </button>
+                </li>
+            @endif
         </div>
     </div>
 </nav>
