a9df3d8112a68c001a002b86e6207aa1ecc21916
Bryan Burchfield <bburchfield@gmail.com>
2021-03-26 15:07:53 -0400

    added chase did api

--- app/Services/DidSwapService.php
+++ app/Services/DidSwapService.php
@@ -2,26 +2,25 @@
 
 namespace App\Services;
 
+use App\Includes\ChaseDataDidApi;
 use App\Models\AreaCode;
 use App\Traits\PhoneTraits;
-use Exception;
-use GuzzleHttp\Client;
 
 class DidSwapService
 {
     use PhoneTraits;
 
-    private $client;
+    private $chaseDataDidApi;
 
     public function __construct()
     {
-        $this->client = new Client();
+        $this->chaseDataDidApi = new ChaseDataDidApi();
     }
 
     public function swapNumber($phone, $dialer_numb, $group_id)
     {
         // try to replace with same NPA
-        list($replaced_by, $swap_error) = $this->swapNumberNpa($phone, $dialer_numb, $group_id);
+        $replaced_by = $this->chaseDataDidApi->swapCallerId($phone, $dialer_numb, $group_id);
 
         if (empty($replaced_by)) {
 
@@ -35,7 +34,7 @@ class DidSwapService
 
                 // loop through till swap succeeds or errors
                 foreach ($alternates as $alternate) {
-                    list($replaced_by, $swap_error) = $this->swapNumberNpa($phone, $dialer_numb, $group_id, $alternate->npa);
+                    list($replaced_by, $swap_error) = $this->chaseDataDidApi->swapCallerId($phone, $dialer_numb, $group_id, $alternate->npa);
                     if (!empty($replaced_by)) {
                         break;
                     }
@@ -43,57 +42,8 @@ class DidSwapService
             }
         }
 
-        return [$replaced_by, $swap_error];
-    }
-
-    private function swapNumberNpa($phone, $dialer_numb, $group_id, $npa = null)
-    {
-        echo "Swapping $phone $npa\n";
-
-        $error = null;
-        $replaced_by = null;
-
-        try {
-            $response = $this->client->get(
-                'https://billing.chasedatacorp.com/DID.aspx',
-                [
-                    'query' => [
-                        'Token' => config('chasedata.did_token'),
-                        'Server' => 'dialer-' . sprintf('%02d', $dialer_numb, 2),
-                        'Number' => $this->formatPhoneTenDigits($phone),
-                        'GroupId' => $group_id,
-                        'Action' => 'swap',
-                        'NPA' => $npa
-                    ]
-                ]
-            );
-        } catch (Exception $e) {
-            $error = 'Swap API failed: ' . $e->getMessage();
-        }
-
-        if (empty($error)) {
-            try {
-                $body = json_decode($response->getBody()->getContents());
-
-                if (isset($body->NewDID)) {
-                    if (!empty($body->NewDID)) {
-                        $replaced_by = $this->formatPhoneElevenDigits($body->NewDID);
-                    } else {
-                        $error = 'No replacement available';
-                    }
-                }
-                if (!empty($body->Error)) {
-                    $error = $body->Error;
-                }
-            } catch (Exception $e) {
-                $error = 'Could not swap number: ' . $e->getMessage();
-            }
-        }
-
         // truncate error just in case
-        if (!empty($error)) {
-            $error = substr($error, 0, 190);
-        }
+        $error = substr($this->chaseDataDidApi->error, 0, 190);
 
         return [$replaced_by, $error];
     }
