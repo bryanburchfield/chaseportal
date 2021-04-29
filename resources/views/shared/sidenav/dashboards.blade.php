3aed03f2a28551cfed15a4c162ee663a066d91a1
Bryan Burchfield <bburchfield@gmail.com>
2021-04-29 11:20:45 -0400

    fixed missing sidenav highlight

--- resources/views/shared/sidenav/dashboards.blade.php
+++ resources/views/shared/sidenav/dashboards.blade.php
@@ -1,12 +1,17 @@
 @extends('shared.sidenav.layout')
 @section('sidenav')
-    <li><a class="update_nav_link" data-path="main" href="#"><i class="fas fa-arrow-circle-left"></i>{{__('widgets.go_back')}}</a></li>
+    <li><a class="update_nav_link" data-path="main" href="#"><i
+                class="fas fa-arrow-circle-left"></i>{{ __('widgets.go_back') }}</a></li>
 
-    <li data-page="inbounddash" @php echo !empty($page['menuitem']) && $page['menuitem'] == 'inbounddash' ? 'class="active"' : ''; @endphp><a href="{{ action('MasterDashController@inboundDashboard') }}"><i class="fas fa-sign-in-alt"></i>{{__('sidenav.inbound')}}</a></li>
-    <li data-page="outbounddash" @php echo !empty($page['menuitem']) && $page['menuitem'] == 'outbounddash' ? 'class="active"' : ''; @endphp><a href="{{ action('MasterDashController@outboundDashboard') }}"><i class="fas fa-sign-out-alt"></i>{{__('sidenav.outbound')}}</a></li>
-	<li data-page="realtimedash" @php echo !empty($page['menuitem']) && $page['menuitem'] == 'realtimedash' ? 'class="active"' : ''@endphp><a href="{{ url('dashboards/realtimeagentdashboard') }}"><i class="fas fa-file-medical-alt"></i>{{__('sidenav.realtimedash')}}</a></li>
+    <li data-page="inbounddash" @php echo !empty($page['menuitem']) && $page['menuitem'] == 'inbounddash' ? 'class="active"' : ''; @endphp><a href="{{ action('MasterDashController@inboundDashboard') }}"><i
+                class="fas fa-sign-in-alt"></i>{{ __('sidenav.inbound') }}</a></li>
+    <li data-page="outbounddash" @php echo !empty($page['menuitem']) && $page['menuitem'] == 'outbounddash' ? 'class="active"' : ''; @endphp><a href="{{ action('MasterDashController@outboundDashboard') }}"><i
+                class="fas fa-sign-out-alt"></i>{{ __('sidenav.outbound') }}</a></li>
+    <li data-page="realtimedash" @php echo !empty($page['menuitem']) && $page['menuitem'] == 'realtimeagentdash' ? 'class="active"' : ''@endphp><a href="{{ url('dashboards/realtimeagentdashboard') }}"><i
+                class="fas fa-file-medical-alt"></i>{{ __('sidenav.realtimedash') }}</a></li>
 
-    @if(Auth::user()->email != 'btmarketing@chasedatacorp.com')
-        <li data-page="trenddash" @php echo !empty($page['menuitem']) &&  $page['menuitem'] == 'trenddash' ? 'class="active"' : ''@endphp><a href="{{ action('MasterDashController@trendDashboard') }}"><i class="fas fa-chart-area"></i>{{__('sidenav.trend_dashboard')}}</a></li>
+    @if (Auth::user()->email != 'btmarketing@chasedatacorp.com')
+        <li data-page="trenddash" @php echo !empty($page['menuitem']) &&  $page['menuitem'] == 'trenddash' ? 'class="active"' : ''@endphp><a href="{{ action('MasterDashController@trendDashboard') }}"><i
+                    class="fas fa-chart-area"></i>{{ __('sidenav.trend_dashboard') }}</a></li>
     @endif
 @endsection
